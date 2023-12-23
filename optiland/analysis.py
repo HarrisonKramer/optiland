7f7e5360cde08ae9d8dd33108c157d01f456d4fb
Kramer <kdanielharrison@gmail.com>
2023-12-23 18:42:59 +0100

    update distortion calculation to use single reference ray

--- optiland/analysis.py
+++ optiland/analysis.py
@@ -308,16 +308,61 @@ class YYbar:
 
 
 class Distortion:
+    # TODO: update for field type = object height
 
-    def __init__(self, optic, wavelengths='all', num_points=10,
+    def __init__(self, optic, wavelengths='all', num_points=128,
                  distortion_type='f-tan'):
         self.optic = optic
         if wavelengths == 'all':
-            wavelength = optic.primary_wavelength
-        self.wavelength = wavelength
+            wavelengths = self.optic.wavelengths.get_wavelengths()
+        self.wavelengths = wavelengths
         self.num_points = num_points
         self.distortion_type = distortion_type
         self.data = self._generate_data()
+
+    def view(self, figsize=(7, 5.5)):
+        _, ax = plt.subplots(figsize=figsize)
+        ax.axvline(x=0, color='k', linewidth=1, linestyle='--')
+
+        field = np.linspace(1e-10, self.optic.fields.max_field,
+                            self.num_points)
+        for k, wavelength in enumerate(self.wavelengths):
+            ax.plot(self.data[k], field, label=f'{wavelength:.4f} µm')
+            ax.set_xlabel('Distortion (%)')
+            ax.set_ylabel('Field')
+
+        current_xlim = plt.xlim()
+        plt.xlim([-max(current_xlim), max(current_xlim)])
+        plt.ylim([0, None])
+        plt.legend(bbox_to_anchor=(1.05, 0.5), loc='center left')
+        plt.show()
+
+    def _generate_data(self):
+        Hx = np.zeros(self.num_points)
+        Hy = np.linspace(1e-10, 1, self.num_points)
+
+        data = []
+        for wavelength in self.wavelengths:
+            self.optic.trace_generic(Hx=Hx, Hy=Hy, Px=0, Py=0,
+                                     wavelength=wavelength)
+            yr = self.optic.surface_group.y[-1, :]
+
+            const = yr[0] / (np.tan(1e-10 *
+                                    np.radians(self.optic.fields.max_field)))
+
+            if self.distortion_type == 'f-tan':
+                yp = const * np.tan(Hy *
+                                    np.radians(self.optic.fields.max_field))
+            elif self.distortion_type == 'f-theta':
+                yp = const * np.tan(Hy *
+                                    np.radians(self.optic.fields.max_field))
+            else:
+                raise ValueError('''Distortion type must be "f-tan" or
+                                 "f-theta"''')
+
+            data.append(100 * (yr - yp) / yp)
+
+        return data
 
 
 class GridDistortion:
