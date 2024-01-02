6d1fada5ec439fcdf29fb396c4ba993a632f2bb0
Kramer <kdanielharrison@gmail.com>
2024-01-02 20:14:33 +0100

    validate aperture type input

--- optiland/aperture.py
+++ optiland/aperture.py
@@ -1,5 +1,8 @@
 class Aperture:
 
     def __init__(self, aperture_type='EPD', value=10):
+        if aperture_type not in ['EPD', 'imageFNO', 'objectNA']:
+            raise ValueError('Aperrture type must be "EPD", "imageFNO", '
+                             '"objectNA"')
         self.ap_type = aperture_type
         self.value = value
