39de77361b6973f6bd41662dffb7e40ef3124000
Kramer <kdanielharrison@gmail.com>
2024-01-14 21:09:14 +0100

    add object space telecentricity option

--- optiland/aperture.py
+++ optiland/aperture.py
@@ -1,8 +1,14 @@
 class Aperture:
 
-    def __init__(self, aperture_type='EPD', value=10):
+    def __init__(self, aperture_type, value, object_space_telecentric=False):
         if aperture_type not in ['EPD', 'imageFNO', 'objectNA']:
-            raise ValueError('Aperrture type must be "EPD", "imageFNO", '
+            raise ValueError('Aperture type must be "EPD", "imageFNO", '
                              '"objectNA"')
+
+        if value in ['EPD', 'imageFNO'] and object_space_telecentric:
+            raise ValueError('Cannot set aperture type to "EPD" or "imageFNO" '
+                             'if lens is telecentric in object space.')
+
         self.ap_type = aperture_type
         self.value = value
+        self.object_space_telecentric = object_space_telecentric
