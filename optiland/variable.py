1c163dd2a7dac2c3afd66f0d6a9a7280ff6bea0c
Kramer <kdanielharrison@gmail.com>
2024-01-17 20:07:28 +0100

    update variable printing

--- optiland/variable.py
+++ optiland/variable.py
@@ -1,10 +1,9 @@
-# TODO: update using Variable classes, like operand.py
 class Variable:
 
-    def __init__(self, optic, variable_type, **kwargs):
+    def __init__(self, optic, type, **kwargs):
         self.__dict__.update(kwargs)
         self.optic = optic
-        self.variable_type = variable_type
+        self.type = type
 
         self._surfaces = self.optic.surface_group
 
@@ -16,38 +15,32 @@ class Variable:
 
     @property
     def value(self):
-        if self.variable_type == 'radius':
+        if self.type == 'radius':
             return self._surfaces.radii[self.surface_number]
-        elif self.variable_type == 'conic':
+        elif self.type == 'conic':
             return self._surfaces.conic[self.surface_number]
-        elif self.variable_type == 'thickness':
+        elif self.type == 'thickness':
             return self._surfaces.get_thickness(self.surface_number)[0]
-        elif self.variable_type == 'index':
+        elif self.type == 'index':
             n = self.optic.n(self.wavelength)
             return n[self.surface_number]
         else:
-            raise ValueError(f'Invalid variable type "{self.variable_type}"')
+            raise ValueError(f'Invalid variable type "{self.type}"')
 
     @property
     def bounds(self):
         '''return the bounds of the variable'''
         return (self.min_val, self.max_val)
 
-    def info(self, number=None):
-        if number is not None:
-            print(f'\tVariable {number}')
-        print(f'\t   Type: {self.variable_type}')
-        print(f'\t   Value: {self.value}')
-
     def update(self, new_value):
         '''update variable to a new value'''
-        if self.variable_type == 'radius':
+        if self.type == 'radius':
             self.optic.set_radius(new_value, self.surface_number)
-        elif self.variable_type == 'conic':
+        elif self.type == 'conic':
             self.optic.set_conic(new_value, self.surface_number)
-        elif self.variable_type == 'thickness':
+        elif self.type == 'thickness':
             self.optic.set_thickness(new_value, self.surface_number)
-        elif self.variable_type == 'index':
+        elif self.type == 'index':
             self.optic.set_thickness(new_value, self.surface_number)
         else:
-            raise ValueError(f'Invalid variable type "{self.variable_type}"')
+            raise ValueError(f'Invalid variable type "{self.type}"')
