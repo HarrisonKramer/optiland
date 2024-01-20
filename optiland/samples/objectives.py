d2bd6f216230ea45e366ca5620755de2c0a548e1
Kramer <kdanielharrison@gmail.com>
2024-01-20 18:18:14 +0100

    add objective_US008879901

--- optiland/samples/objectives.py
+++ optiland/samples/objectives.py
@@ -157,3 +157,51 @@ def reverse_telephoto():
     lens.update_paraxial()
 
     return lens
+
+
+def objective_US008879901():
+    """An optimized design with starting point US008879901"""
+    lens = optic.Optic()
+    lens.surface_group.surfaces = []
+
+    lens.add_surface(index=0, radius=np.inf, thickness=np.inf)
+    lens.add_surface(index=1, radius=47.07125235, thickness=5.29811826, material='N-LAF32')
+    lens.add_surface(index=2, radius=184.28171667, thickness=0.6)
+    lens.add_surface(index=3, radius=29.92177645, thickness=7.13654863, material='H-ZLAF52A')
+    lens.add_surface(index=4, radius=50.4992638, thickness=2.0)
+    lens.add_surface(index=5, radius=60.5004845, thickness=0.99941671, material='E-SF1')
+    lens.add_surface(index=6, radius=17.72638376, thickness=9.9)
+    lens.add_surface(index=7, radius=np.inf, thickness=8.7, is_stop=True)
+    lens.add_surface(index=8, radius=-17.49862241, thickness=1.29934579, material=('SF4', 'hikari'))
+    lens.add_surface(index=9, radius=1000.00000019, thickness=8.44325264, material='M-TAF1')
+    lens.add_surface(index=10, radius=-28.00122422, thickness=0.1)
+    lens.add_surface(index=11, radius=-141.99976777, thickness=6.79950254, material='M-TAF1')
+    lens.add_surface(index=12, radius=-35.94103045, thickness=0.516)
+    lens.add_surface(index=13, radius=92.00034667, thickness=3.29901361, material='Q-LAFPH1S')
+    lens.add_surface(index=14, radius=-277.85210888, thickness=2.13)
+    lens.add_surface(index=15, radius=-157.24588662, thickness=1.29980422, material='S-FSL5')
+    lens.add_surface(index=16, radius=740.47397742, thickness=0.25)
+    lens.add_surface(index=17, radius=19.91929498, thickness=5.59345688, material='J-LASF015')
+    lens.add_surface(index=18, radius=36.48852623, thickness=0.574)
+    lens.add_surface(index=19, radius=45.97532235, thickness=1.00045731, material='E-SF1')
+    lens.add_surface(index=20, radius=16.39521847, thickness=2.951)
+    lens.add_surface(index=21, radius=33.86131631, thickness=3.22444231, material='H-LAK52')
+    lens.add_surface(index=22, radius=np.inf, thickness=8.0)
+    lens.add_surface(index=23, radius=np.inf, thickness=4.0, material='H-LAK52')
+    lens.add_surface(index=24, radius=np.inf, thickness=3.15317838)
+    lens.add_surface(index=25)
+
+    lens.set_aperture(aperture_type='imageFNO', value=2.0)
+
+    lens.set_field_type(field_type='angle')
+    lens.add_field(0.0)
+    lens.add_field(7.574)
+    lens.add_field(10.82)
+
+    lens.add_wavelength(value=0.4861327)
+    lens.add_wavelength(value=0.5875618, is_primary=True)
+    lens.add_wavelength(value=0.6562725)
+
+    lens.update_paraxial()
+
+    return lens
