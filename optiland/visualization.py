370488909734b6f5f5a6e2bb623328eb2e254fad
Kramer <kdanielharrison@gmail.com>
2024-07-07 22:12:02 +0200

    add docs

--- optiland/visualization.py
+++ optiland/visualization.py
@@ -1,3 +1,15 @@
+"""Optiland Distribution Module
+
+This module provides visualization tools for optical systems using VTK and
+Matplotlib. It includes the `LensViewer` class, which allows for the
+visualization of lenses, rays, and their interactions within an optical system.
+The module supports plotting rays with different distributions, wavelengths,
+and through various fields of view. It also visualizes the surfaces of the
+optical elements, providing insights into the design and performance of the
+system.
+
+Kramer Harrison, 2023
+"""
 import vtkmodules.vtkRenderingOpenGL2  # noqa
 from vtkmodules.vtkFiltersSources import vtkLineSource
 from vtkmodules.vtkCommonCore import vtkPoints
@@ -24,6 +36,29 @@ from optiland.rays import RealRays
 
 
 class LensViewer:
+    """
+    A class for visualizing optical systems and traced rays.
+
+    Args:
+        optic: An instance of the `Optic` class representing the optical
+            system.
+
+    Attributes:
+        optic: An instance of the `Optic` class representing the optical
+            system.
+        _real_ray_extent: An array storing the maximum extent of rays for each
+            surface.
+
+    Methods:
+        view: Visualizes the lenses and ray tracing.
+        _plot_all_surfaces: Plots all the surfaces of the optical system.
+        _plot_rays: Plots the rays for the given fields and wavelengths.
+        _plot_lens: Plots a lens.
+        _plot_surface: Plots a surface.
+        _plot_line: Plots a line.
+        _get_surface_extent: Returns the extent of a surface.
+    """
+
     # TODO: does not plot simple singlet correctly. Rays do not converge..
     # TODO: does not plot lenses with a single field correctly..
 
@@ -35,6 +70,18 @@ class LensViewer:
 
     def view(self, fields='all', wavelengths='primary', num_rays=3,
              distribution='line_y', figsize=(10, 4)):
+        """
+        Visualizes the lenses and traced rays.
+
+        Args:
+            fields: The fields at which to trace the rays. Default is 'all'.
+            wavelengths: The wavelengths at which to trace the rays.
+                Default is 'primary'.
+            num_rays: The number of rays to trace for each field and
+                wavelength. Default is 3.
+            distribution: The distribution of the rays. Default is 'line_y'.
+            figsize: The size of the figure. Default is (10, 4).
+        """
         _, self.ax = plt.subplots(figsize=figsize)
         self._plot_rays(fields=fields, wavelengths=wavelengths,
                         num_rays=num_rays, distribution=distribution)
@@ -44,6 +91,9 @@ class LensViewer:
         plt.show()
 
     def _plot_all_surfaces(self):
+        """
+        Plots all the surfaces of the optical system.
+        """
         n = self.optic.n()
 
         for k in range(1, self.optic.surface_group.num_surfaces-1):
@@ -109,6 +159,17 @@ class LensViewer:
 
     def _plot_rays(self, fields='all', wavelengths='primary', num_rays=3,
                    distribution='line_y'):
+        """
+        Plots the rays for the given fields and wavelengths.
+
+        Args:
+            fields: The fields at which to trace the rays. Default is 'all'.
+            wavelengths: The wavelengths at which to trace the rays.
+                Default is 'primary'.
+            num_rays: The number of rays to trace for each field and
+                wavelength. Default is 3.
+            distribution: The distribution of the rays. Default is 'line_y'.
+        """
         if fields == 'all':
             fields = self.optic.fields.get_field_coords()
 
@@ -142,18 +203,50 @@ class LensViewer:
                     self._plot_line(xk, yk, zk, f'C{i}')
 
     def _plot_lens(self, y, z):
+        """
+        Plots a lens.
+
+        Args:
+            y: The y-coordinates of the lens profile.
+            z: The z-coordinates of the lens profile.
+        """
         vertices = np.column_stack((z, y))
         polygon = Polygon(vertices, closed=True, facecolor='lightgray',
                           edgecolor='gray')
         self.ax.add_patch(polygon)
 
     def _plot_surface(self, y, z):
+        """
+        Plots a surface.
+
+        Args:
+            y: The y-coordinates of the lens profile.
+            z: The z-coordinates of the lens profile.
+        """
         self.ax.plot(z, y, 'gray')
 
     def _plot_line(self, x, y, z, color):
+        """
+        Plots a line.
+
+        Args:
+            x: The x-coordinates of the line.
+            y: The y-coordinates of the line.
+            z: The z-coordinates of the line.
+            color: The color of the line.
+        """
         self.ax.plot(z, y, color, linewidth=1)
 
     def _get_surface_extent(self, surf_index):
+        """
+        Returns the extent of a surface.
+
+        Args:
+            surf_index: The index of the surface.
+
+        Returns:
+            The y-coordinates representing the extent of the surface.
+        """
         x = np.zeros(256)
         y = np.linspace(-self._real_ray_extent[surf_index],
                         self._real_ray_extent[surf_index], 256)
@@ -167,6 +260,21 @@ class LensViewer:
 
 
 class LensViewer3D(LensViewer):
+    """
+    A class for visualizing optical systems in 3D.
+
+    Args:
+        optic (OpticalSystem): The optical system to visualize.
+
+    Attributes:
+        _rgb_colors (list): A list of RGB colors used for visualization.
+
+    Methods:
+        view: Visualize the optical system in 3D.
+        _plot_lens: Plot a lens in the 3D visualization.
+        _plot_surface: Plot a surface in the 3D visualization.
+        _plot_line: Plot a line in the 3D visualization.
+    """
 
     def __init__(self, optic):
         super().__init__(optic)
@@ -185,6 +293,21 @@ class LensViewer3D(LensViewer):
 
     def view(self, fields='all', wavelengths='primary', num_rays=2,
              distribution='hexapolar', figsize=(1200, 800)):
+        """
+        Visualize the optical system in 3D.
+
+        Args:
+            fields (str or list, optional): The fields to visualize.
+                Defaults to 'all'.
+            wavelengths (str or list, optional): The wavelengths to visualize.
+                Defaults to 'primary'.
+            num_rays (int, optional): The number of rays (or rings) to trace.
+                Defaults to 2.
+            distribution (str, optional): The distribution of rays.
+                Defaults to 'hexapolar'.
+            figsize (tuple, optional): The size of the figure.
+                Defaults to (1200, 800).
+        """
         self.renderer = vtkRenderer()
         renWin = vtkRenderWindow()
         renWin.AddRenderer(self.renderer)
@@ -221,6 +344,15 @@ class LensViewer3D(LensViewer):
         iren.Start()
 
     def _plot_lens(self, y, z, make_transparent=True):
+        """
+        Plot a lens in the 3D visualization.
+
+        Args:
+            y (list): The y-coordinates of the lens points.
+            z (list): The z-coordinates of the lens points.
+            make_transparent (bool, optional): Whether to make the lens
+                transparent. Defaults to True.
+        """
         pts = [(0, yi, zi) for yi, zi in zip(y, z)]
 
         points = vtkPoints()
@@ -256,9 +388,25 @@ class LensViewer3D(LensViewer):
         self.renderer.AddActor(surfaceActor)
 
     def _plot_surface(self, y, z):
+        """
+        Plot a surface in the 3D visualization.
+
+        Args:
+            y (list): The y-coordinates of the surface points.
+            z (list): The z-coordinates of the surface points.
+        """
         self._plot_lens(y, z, make_transparent=False)
 
     def _plot_line(self, x, y, z, color):
+        """
+        Plot a line in the 3D visualization.
+
+        Args:
+            x (list): The x-coordinates of the line points.
+            y (list): The y-coordinates of the line points.
+            z (list): The z-coordinates of the line points.
+            color (str): The color of the line.
+        """
         color = self._rgb_colors[int(color[1:]) % 10]
         for k in range(1, len(x)):
             p0 = [x[k-1], y[k-1], z[k-1]]
