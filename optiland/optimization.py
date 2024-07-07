e74b9d60e7f7c88e3ad0f37d6cef73ece4b500ea
Kramer <kdanielharrison@gmail.com>
2024-07-07 21:18:33 +0200

    add docs

--- optiland/optimization.py
+++ optiland/optimization.py
@@ -1,3 +1,13 @@
+"""
+Optiland Optimization Module
+
+This module provides classes for defining and solving optimization problems
+for Optiland Optic instances. It includes an OptimizationProblem class for
+defining the problem, and several optimizer classes for solving the problem
+using different optimization algorithms.
+
+Kramer Harrison, 2024
+"""
 import warnings
 import numpy as np
 import pandas as pd
@@ -7,33 +17,56 @@ from optiland.operand import Operand
 
 
 class OptimizationProblem:
+    """
+    Represents an optimization problem.
+
+    Attributes:
+        operands (list): List of operands in the merit function.
+        variables (list): List of variables in the merit function.
+        initial_value (float): Initial value of the merit function.
+
+    Methods:
+        add_operand: Add an operand to the merit function.
+        add_variable: Add a variable to the merit function.
+        fun_array: Array of operand weighted deltas squared, where the delta
+            is the difference between the current and target value.
+        sum_squared: Sum of squared operand weighted deltas.
+        rss: Root Sum of Squares (RSS) of the current merit function.
+        operand_info: Print information about the operands in the merit
+            function.
+        variable_info: Print information about the variables in the merit
+            function.
+        info: Print information about the merit function, including operand
+            and variable info.
+    """
 
     def __init__(self):
         self.operands = []
         self.variables = []
-
         self.initial_value = 0.0
 
     def add_operand(self, operand_type, target, weight=1, input_data={}):
-        '''add an operand to the merit function'''
+        """Add an operand to the merit function"""
         self.operands.append(Operand(operand_type, target, weight, input_data))
 
     def add_variable(self, optic, variable_type, **kwargs):
-        '''add a variable to the merit function'''
+        """Add a variable to the merit function"""
         self.variables.append(Variable(optic, variable_type, **kwargs))
 
     def fun_array(self):
-        '''Array of operand weighted deltas squared'''
+        """Array of operand weighted deltas squared"""
         return np.array([op.fun() for op in self.operands])**2
 
     def sum_squared(self):
+        """Calculate the sum of squared operand weighted deltas"""
         return np.sum(self.fun_array())
 
     def rss(self):
-        '''RSS of current merit function'''
+        """RSS of current merit function"""
         return np.sqrt(self.sum_squared())
 
     def operand_info(self):
+        """Print information about the operands in the merit function"""
         data = {'Operand Type': [op.type.replace('_', ' ')
                                  for op in self.operands],
                 'Target': [op.target for op in self.operands],
@@ -48,6 +81,7 @@ class OptimizationProblem:
         print(df.to_markdown(headers='keys', tablefmt='psql'))
 
     def variable_info(self):
+        """Print information about the variables in the merit function."""
         data = {'Variable Type': [var.type for var in self.variables],
                 'Surface': [var.surface_number for var in self.variables],
                 'Value': [var.value for var in self.variables],
@@ -58,7 +92,7 @@ class OptimizationProblem:
         print(df.to_markdown(headers='keys', tablefmt='psql'))
 
     def info(self):
-        '''Print info about merit function'''
+        """Print information about the merit function."""
         current_value = self.sum_squared()
 
         if self.initial_value == 0.0:
@@ -77,7 +111,23 @@ class OptimizationProblem:
 
 
 class OptimizerGeneric:
-    # TODO: add norm and un-norm of parameters to make more robust
+    """
+    Generic optimizer class for solving optimization problems.
+
+    Args:
+        problem (OptimizationProblem): The optimization problem to be solved.
+
+    Attributes:
+        problem (OptimizationProblem): The optimization problem to be solved.
+        _x (list): List to store the values of the variables during
+            optimization.
+
+    Methods:
+        optimize(maxiter=1000, disp=True, tol=1e-3): Optimize the problem
+            using the specified parameters.
+        undo(): Undo the last optimization step.
+        _fun(x): Internal function to evaluate the objective function.
+    """
 
     def __init__(self, problem: OptimizationProblem):
         self.problem = problem
@@ -87,7 +137,19 @@ class OptimizerGeneric:
             self.problem.initial_value = self.problem.sum_squared()
 
     def optimize(self, maxiter=1000, disp=True, tol=1e-3):
+        """
+        Optimize the problem using the specified parameters.
 
+        Args:
+            maxiter (int, optional): Maximum number of iterations.
+                Default is 1000.
+            disp (bool, optional): Whether to display optimization information.
+                Default is True.
+            tol (float, optional): Tolerance for convergence. Default is 1e-3.
+
+        Returns:
+            result (OptimizeResult): The optimization result.
+        """
         x0 = [var.value for var in self.problem.variables]
         self._x.append(x0)
         bounds = tuple([var.bounds for var in self.problem.variables])
@@ -104,6 +166,9 @@ class OptimizerGeneric:
         return result
 
     def undo(self):
+        """
+        Undo the last optimization step.
+        """
         if len(self._x) > 0:
             x0 = self._x[-1]
             for idvar, var in enumerate(self.problem.variables):
@@ -111,6 +176,15 @@ class OptimizerGeneric:
             self._x.pop(-1)
 
     def _fun(self, x):
+        """
+        Internal function to evaluate the objective function.
+
+        Args:
+            x (array-like): The values of the variables.
+
+        Returns:
+            rss (float): The residual sum of squares.
+        """
         for idvar, var in enumerate(self.problem.variables):
             var.update(x[idvar])
         funs = np.array([op.fun() for op in self.problem.operands])
@@ -122,11 +196,38 @@ class OptimizerGeneric:
 
 
 class LeastSquares(OptimizerGeneric):
+    """
+    LeastSquares optimizer class for solving optimization problems using the
+    least squares method.
+
+    Args:
+        problem (OptimizationProblem): The optimization problem to be solved.
+
+    Attributes:
+        problem (OptimizationProblem): The optimization problem to be solved.
+
+    Methods:
+        optimize(maxiter=None, disp=False, tol=1e-3): Optimize the problem
+            using the least squares method.
+    """
 
     def __init__(self, problem: OptimizationProblem):
         super().__init__(problem)
 
     def optimize(self, maxiter=None, disp=False, tol=1e-3):
+        """
+        Optimize the problem using the least squares method.
+
+        Args:
+            maxiter (int, optional): Maximum number of iterations.
+                Defaults to None.
+            disp (bool, optional): Whether to display optimization progress.
+                Defaults to False.
+            tol (float, optional): Tolerance for termination. Defaults to 1e-3.
+
+        Returns:
+            result: The optimization result.
+        """
 
         x0 = [var.value for var in self.problem.variables]
         self._x.append(x0)
@@ -154,12 +255,32 @@ class LeastSquares(OptimizerGeneric):
 
 
 class DualAnnealing(OptimizerGeneric):
+    """
+    DualAnnealing is an optimizer that uses the dual annealing algorithm
+    to find the minimum of an optimization problem.
+
+    Parameters:
+        problem (OptimizationProblem): The optimization problem to be solved.
+
+    Methods:
+        optimize(maxiter=1000, disp=True): Runs the dual annealing algorithm
+            to optimize the problem and returns the result.
+    """
 
     def __init__(self, problem: OptimizationProblem):
         super().__init__(problem)
 
     def optimize(self, maxiter=1000, disp=True):
+        """
+        Runs the dual annealing algorithm to optimize the problem.
 
+        Parameters:
+            maxiter (int): Maximum number of iterations.
+            disp (bool): Whether to display the optimization process.
+
+        Returns:
+            result: The result of the optimization.
+        """
         x0 = [var.value for var in self.problem.variables]
         self._x.append(x0)
         bounds = tuple([var.bounds for var in self.problem.variables])
@@ -176,12 +297,43 @@ class DualAnnealing(OptimizerGeneric):
 
 
 class DifferentialEvolution(OptimizerGeneric):
+    """
+    Differential Evolution optimizer for solving optimization problems.
+
+    Args:
+        problem (OptimizationProblem): The optimization problem to be solved.
+
+    Methods:
+        optimize(maxiter=1000, disp=True, workers=-1): Runs the differential
+            evolution optimization algorithm.
+    """
 
     def __init__(self, problem: OptimizationProblem):
+        """
+        Initializes a new instance of the DifferentialEvolution class.
+
+        Args:
+            problem (OptimizationProblem): The optimization problem to be
+                solved.
+        """
         super().__init__(problem)
 
     def optimize(self, maxiter=1000, disp=True, workers=-1):
+        """
+        Runs the differential evolution optimization algorithm.
 
+        Args:
+            maxiter (int): Maximum number of iterations.
+            disp (bool): Set to True to display status messages.
+            workers (int): Number of parallel workers to use. Set to -1 to use
+                all available processors.
+
+        Returns:
+            result (OptimizeResult): The optimization result.
+
+        Raises:
+            ValueError: If any variable in the problem does not have bounds.
+        """
         x0 = [var.value for var in self.problem.variables]
         self._x.append(x0)
         bounds = tuple([var.bounds for var in self.problem.variables])
