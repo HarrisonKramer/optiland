adad0e373691d478652cd145641fb74c8b114f48
Kramer <kdanielharrison@gmail.com>
2024-07-07 16:07:14 +0200

    add docs

--- optiland/aperture.py
+++ optiland/aperture.py
@@ -1,4 +1,28 @@
+"""Optiland Aperture Module
+
+This module provides a class for representing apertures in lens systems.
+
+Kramer Harrison, 2024
+"""
+
+
 class Aperture:
+    """
+    Represents an aperture used in optics.
+
+    Attributes:
+        ap_type (str): The type of the aperture. Must be one of 'EPD',
+            'imageFNO', or 'objectNA'.
+        value (float): The value associated with the aperture.
+        object_space_telecentric (bool): Indicates whether the lens is
+            telecentric in object space.
+
+    Raises:
+        ValueError: If the aperture type is not one of 'EPD', 'imageFNO',
+            or 'objectNA', or if the aperture type is 'EPD' or 'imageFNO'
+            and the lens is telecentric in object space.
+
+    """
 
     def __init__(self, aperture_type, value, object_space_telecentric=False):
         if aperture_type not in ['EPD', 'imageFNO', 'objectNA']:
