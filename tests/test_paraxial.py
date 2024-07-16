3baa9279766d10e72632058eb813a2e434e717f7
Kramer <kdanielharrison@gmail.com>
2024-07-16 20:28:43 +0200

    draft initial paraxial unit tests

--- tests/test_paraxial.py
+++ tests/test_paraxial.py
@@ -1,233 +1,314 @@
 import pytest
-import numpy as np
-from optiland.samples.simple import Edmund_49_847, SingletStopSurf2
-from optiland.samples.objectives import (
-    Cooke_triplet,
-    double_Gauss,
-    reverse_telephoto
-)
+from optiland.optic import Optic
+from optiland.paraxial import Paraxial
 from optiland.samples.eyepieces import EyepieceErfle
-
-
-@pytest.mark.parametrize('optic, target',
-                         [(Edmund_49_847, ),
-                          (SingletStopSurf2, ),
-                          (Cooke_triplet, ),
-                          (double_Gauss, ),
-                          (reverse_telephoto, ),
-                          (EyepieceErfle, )])
-def test_f1(optic, target):
-    assert np.allclose(optic.paraxial.f1(), target)
-
-
-def test_f2(optic, target):
-    assert np.allclose(optic.paraxial.f2(), target)
-
-
-def test_F1(optic, target):
-    assert np.allclose(optic.paraxial.f2(), target)
-
-
-def test_F2(optic, target):
-    assert np.allclose(optic.paraxial.f2(), target)
-
-
-def test_P1(optic, target):
-    assert np.allclose(optic.paraxial.f2(), target)
-
-
-def test_P2(optic, target):
-    assert np.allclose(optic.paraxial.f2(), target)
-
-
-def test_N1(optic, target):
-    assert np.allclose(optic.paraxial.f2(), target)
-
-
-def test_N2(optic, target):
-    assert np.allclose(optic.paraxial.f2(), target)
-
-
-def test_EPD(optic, target):
-    assert np.allclose(optic.paraxial.f2(), target)
-
-
-def test_EPL(optic, target):
-    assert np.allclose(optic.paraxial.f2(), target)
-
-
-def test_XPD(optic, target):
-    assert np.allclose(optic.paraxial.f2(), target)
-
-
-def test_XPL(optic, target):
-    assert np.allclose(optic.paraxial.f2(), target)
-
-
-def test_FNO(optic, target):
-    assert np.allclose(optic.paraxial.f2(), target)
-
-
-def test_invariant(optic, target):
-    assert np.allclose(optic.paraxial.f2(), target)
-
-
-
-# class TestSingletObjInfinityStop1(unittest.TestCase):
-#     def setUp(self):
-#         air = materials.IdealMaterial(n=1, k=0)
-#         mat = materials.IdealMaterial(n=1.5185223876, k=0)
-
-#         self.lens = optic.Optic()
-
-#         self.lens.surface_group.surfaces = []
-
-#         self.lens.set_aperture(aperture_type='EPD', value=10)
-#         self.lens.set_field_type(field_type='angle')
-
-#         self.lens.add_field(y=0)
-#         self.lens.add_field(y=7)
-#         self.lens.add_field(y=10)
-
-#         self.lens.add_surface(index=0, thickness=np.inf, radius=np.inf, conic=0, material=air)
-#         self.lens.add_surface(index=1, thickness=10, radius=150, conic=0, material=mat, is_stop=True)
-#         self.lens.add_surface(index=2, thickness=140, radius=-250, conic=0, material=air)
-#         self.lens.add_surface(index=3)
-
-#         self.lens.add_wavelength(0.55, is_primary=True)
-
-#     def test_f1(self):
-#         self.assertAlmostEquals(self.lens.paraxial.f1(), -182.358953, places=5)
-
-#     def test_f2(self):
-#         self.assertAlmostEquals(self.lens.paraxial.f2(), 182.358953, places=5)
-
-#     def test_EPD(self):
-#         self.assertAlmostEquals(self.lens.paraxial.EPD(), 10.0, places=5)
-
-#     def test_EPL(self):
-#         self.assertAlmostEquals(self.lens.paraxial.EPL(), 0.0, places=5)
-
-#     def test_XPD(self):
-#         self.assertAlmostEquals(self.lens.paraxial.XPD(), 10.13848, places=5)
-
-#     def test_XPL(self):
-#         self.assertAlmostEquals(self.lens.paraxial.XPL(), -146.6765413, places=5)
-
-#     def test_magnification(self):
-#         self.assertAlmostEqual(self.lens.paraxial.magnification(), 0, places=5)
-
-#     def test_F1(self):
-#         self.assertAlmostEqual(self.lens.paraxial.F1(), -179.868184, places=5)
-
-#     def test_F2(self):
-#         self.assertAlmostEquals(self.lens.paraxial.F2(), 38.207672, places=5)
-
-#     def test_P1(self):
-#         self.assertAlmostEquals(self.lens.paraxial.P1(), 2.490769, places=5)
-
-#     def test_P2(self):
-#         self.assertAlmostEquals(self.lens.paraxial.P2(), -144.151281, places=5)
-
-#     def test_N1(self):
-#         self.assertAlmostEquals(self.lens.paraxial.N1(), 2.490769, places=5)
-
-#     def test_N2(self):
-#         self.assertAlmostEquals(self.lens.paraxial.N2(), -144.151281, places=5)
-
-#     def test_invariant(self):
-#         self.assertAlmostEqual(self.lens.paraxial.invariant(), -0.8816349035423249, places=5)
-
-#     def test_marginal_ray(self):
-#         y, u = self.lens.paraxial.marginal_ray()
-#         y_target = np.array([[5.], [5.], [4.8861783025], [1.0475951700]])
-#         self.assertTrue(np.max(np.abs(y - y_target)) < 0.00001)
-#         u_target = np.array([[0.], [-0.01138217], [-0.02741845], [-0.02741845]])
-#         self.assertTrue(np.max(np.abs(u - u_target)) < 0.00001)
-
-#     def test_chief_ray(self):
-#         y, u = self.lens.paraxial.chief_ray()
-#         y_target = np.array([[0.0], [0.0], [1.1611747192], [25.509778769]])
-#         self.assertTrue(np.max(np.abs(y - y_target)) < 0.00001)
-#         u_target = np.array([[0.17632698], [0.11611747], [0.1739186], [0.1739186]])
-#         self.assertTrue(np.max(np.abs(u - u_target)) < 0.00001)
-
-
-# class TestSingletObjInfinityStop2(unittest.TestCase):
-#     def setUp(self):
-#         air = materials.IdealMaterial(n=1, k=0)
-#         mat = materials.IdealMaterial(n=1.5185223876, k=0)
-
-#         self.lens = optic.Optic()
-
-#         self.lens.surface_group.surfaces = []
-
-#         self.lens.set_aperture(aperture_type='EPD', value=10)
-#         self.lens.set_field_type(field_type='angle')
-
-#         self.lens.add_field(y=0)
-#         self.lens.add_field(y=7)
-#         self.lens.add_field(y=10)
-
-#         self.lens.add_surface(index=0, thickness=np.inf, radius=np.inf, conic=0, material=air)
-#         self.lens.add_surface(index=1, thickness=10, radius=150, conic=0, material=mat)
-#         self.lens.add_surface(index=2, thickness=140, radius=-250, conic=0, material=air, is_stop=True)
-#         self.lens.add_surface(index=3)
-
-#         self.lens.add_wavelength(0.55, is_primary=True)
-
-#     def test_f1(self):
-#         self.assertAlmostEquals(self.lens.paraxial.f1(), -182.358953, places=5)
-
-#     def test_f2(self):
-#         self.assertAlmostEquals(self.lens.paraxial.f2(), 182.358953, places=5)
-
-#     def test_EPD(self):
-#         self.assertAlmostEquals(self.lens.paraxial.EPD(), 10.0, places=5)
-
-#     def test_EPL(self):
-#         self.assertAlmostEquals(self.lens.paraxial.EPL(), 6.738752, places=5)
-
-#     def test_XPD(self):
-#         self.assertAlmostEquals(self.lens.paraxial.XPD(), 9.772357, places=5)
-
-#     def test_XPL(self):
-#         self.assertAlmostEquals(self.lens.paraxial.XPL(), -140.0, places=5)
-
-#     def test_magnification(self):
-#         self.assertAlmostEqual(self.lens.paraxial.magnification(), 0, places=5)
-
-#     def test_F1(self):
-#         self.assertAlmostEqual(self.lens.paraxial.F1(), -179.868184, places=5)
-
-#     def test_F2(self):
-#         self.assertAlmostEquals(self.lens.paraxial.F2(), 38.207672, places=5)
-
-#     def test_P1(self):
-#         self.assertAlmostEquals(self.lens.paraxial.P1(), 2.490769, places=5)
-
-#     def test_P2(self):
-#         self.assertAlmostEquals(self.lens.paraxial.P2(), -144.151281, places=5)
-
-#     def test_N1(self):
-#         self.assertAlmostEquals(self.lens.paraxial.N1(), 2.490769, places=5)
-
-#     def test_N2(self):
-#         self.assertAlmostEquals(self.lens.paraxial.N2(), -144.151281, places=5)
-
-#     def test_invariant(self):
-#         self.assertAlmostEqual(self.lens.paraxial.invariant(), -0.8816349035423249, places=5)
-
-#     def test_marginal_ray(self):
-#         y, u = self.lens.paraxial.marginal_ray()
-#         y_target = np.array([[5.], [5.], [4.8861783], [1.04759517]])
-#         self.assertTrue(np.max(np.abs(y - y_target)) < 0.00001)
-#         u_target = np.array([[0.], [-0.01138217], [-0.02741845], [-0.02741845]])
-#         self.assertTrue(np.max(np.abs(u - u_target)) < 0.00001)
-
-#     def test_chief_ray(self):
-#         y, u = self.lens.paraxial.chief_ray()
-#         y_target = np.array([[-1.18822385], [-1.18822385], [0], [25.26082326]])
-#         self.assertTrue(np.max(np.abs(y - y_target)) < 0.00001)
-#         u_target = np.array([[0.17632698], [0.11882239], [0.18043445], [0.18043445]])
-#         self.assertTrue(np.max(np.abs(u - u_target)) < 0.00001)
+from optiland.samples.telescopes import HubbleTelescope
+from optiland.samples.infrared import InfraredTriplet, InfraredTripletF4
+from optiland.samples.simple import (
+    Edmund_49_847,
+    SingletStopSurf2,
+    TelescopeDoublet,
+    CementedAchromat
+)
+from optiland.samples.objectives import (
+    TripletTelescopeObjective,
+    CookeTriplet,
+    DoubleGauss,
+    ReverseTelephoto,
+    ObjectiveUS008879901,
+    TelescopeObjective48Inch,
+    HeliarLens,
+    TessarLens,
+    LensWithFieldCorrector,
+    PetzvalLens,
+    Telephoto
+)
+
+
+def get_optic_data():
+    return {
+        (EyepieceErfle(), {
+            "f1": -79.68780891169393,
+            "f2": 79.68780891169399,
+            "F1": -18.953576951285132,
+            "F2": 0.41237074219460346,
+            "P1": 60.734231960408806,
+            "N1": 60.73423196040886,
+            "EPL": 0.0,
+            "EPD": 4.0,
+            "XPL": -334.6244882880523,
+            "XPD": 16.81747125970136,
+            "FNO": 19.921952227923498,
+            "invariant": -0.7279404685324046
+        }),
+        (HubbleTelescope(), {
+            "f1": -57600.08099840297,
+            "f2": 57600.080998403595,
+            "F1": -471891.9369498712,
+            "F2": 0.016864237529848742,
+            "P1": -414291.8559514682,
+            "N1": -414291.8559514676,
+            "EPL": 4910.01016,
+            "EPD": 2400,
+            "XPL": -6958.363551642134,
+            "XPD": 289.93210961932573,
+            "FNO": 24.000033749334833,
+            "invariant": -3.1415998309883313
+        }),
+        (InfraredTriplet(), {
+            "f1": -10.002605806173715,
+            "f2": 10.002605806173719,
+            "F1": -1.5280656498833074,
+            "F2": 0.007908340354459628,
+            "P1": 8.474540156290407,
+            "N1": 8.474540156290411,
+            "EPL": 0.0,
+            "EPD": 5.001302903086859,
+            "XPL": -65.46841652915609,
+            "XPD": 32.73816243475526,
+            "FNO": 2,
+            "invariant": -0.17486258378834374
+        }),
+        (InfraredTripletF4(), {
+            "f1": -3.978671113840911,
+            "f2": 3.978671113840912,
+            "F1": -1.152309312063933,
+            "F2": -0.028193159257657953,
+            "P1": 2.8263618017769785,
+            "N1": 2.8263618017769794,
+            "EPL": 0.3429040402261547,
+            "EPD": 0.994667778460228,
+            "XPL": -10.615193207021356,
+            "XPD": 2.6467500119409237,
+            "FNO": 4,
+            "invariant": -0.0304182437549445
+        }),
+        (Edmund_49_847(), {
+            "f1": -25.397595913429043,
+            "f2": 25.397595913429043,
+            "F1": -25.397595913429043,
+            "F2": -0.004588085333006088,
+            "P1": 0.0,
+            "N1": 0.0,
+            "EPL": 0.0,
+            "EPD": 25.4,
+            "XPL": -25.402183998762045,
+            "XPD": 25.4,
+            "FNO": 0.9999053509224033,
+            "invariant": -3.1664656361083945
+        }),
+        (SingletStopSurf2(), {
+            "f1": -101.5018259488552,
+            "f2": 101.50182594885518,
+            "F1": -102.13671549195077,
+            "F2": 2.2556217861906513,
+            "P1": -0.6348895430955679,
+            "N1": -0.6348895430955821,
+            "EPL": 6.318809406260012,
+            "EPD": 25.0,
+            "XPL": -92.7383463,
+            "XPD": 23.39710817962435,
+            "FNO": 4.060073037954208,
+            "invariant": -1.0936082940740497
+        }),
+        (TelescopeDoublet(), {
+            "f1": -48.01075268520558,
+            "f2": 48.01075268520557,
+            "F1": -47.80852706120782,
+            "F2": -5.330264306069928e-15,
+            "P1": 0.20222562399776223,
+            "N1": 0.20222562399774802,
+            "EPL": 0.0,
+            "EPD": 6.001344085650696,
+            "XPL": -48.21383370478891,
+            "XPD": 6.026729213098612,
+            "FNO": 8.0,
+            "invariant": -0.05237692533580375
+        }),
+        (CementedAchromat(), {
+            "f1": -20.005995274922704,
+            "f2": 20.005995274922707,
+            "F1": -19.932205229642438,
+            "F2": 2.221111658476128e-15,
+            "P1": 0.07379004528026556,
+            "N1": 0.07379004528026911,
+            "EPL": 0.0,
+            "EPD": 3.3343325458204514,
+            "XPL": -20.080058494732317,
+            "XPD": 3.3466764157887203,
+            "FNO": 6,
+            "invariant": -0.14585814909241152
+        }),
+        (TripletTelescopeObjective(), {
+            "f1": -100.00126142179896,
+            "f2": 100.00126142179887,
+            "F1": -115.31444160788848,
+            "F2": 0.0015145573975955757,
+            "P1": -15.313180186089525,
+            "N1": -15.31318018608961,
+            "EPL": 0.0,
+            "EPD": 35.714736222071025,
+            "XPL": -86.72008029674481,
+            "XPD": 30.97199816219372,
+            "FNO": 2.8,
+            "invariant": -0.3117015198252071
+        }),
+        (CookeTriplet(), {
+            "f1": -49.99978307143189,
+            "f2": 49.9997830714319,
+            "F1": -37.345672974426165,
+            "F2": 0.20707755895880453,
+            "P1": 12.654110097005727,
+            "N1": 12.654110097005734,
+            "EPL": 11.512158673746795,
+            "EPD": 10,
+            "XPL": -50.961347703805274,
+            "XPD": 10.233729452318345,
+            "FNO": 4.99997830714319,
+            "invariant": -1.8198511713310106
+        }),
+        (DoubleGauss(), {
+            "f1": -100.00374787083722,
+            "f2": 100.00374787083722,
+            "F1": -29.32075813267318,
+            "F2": 1.0239192327178702e-05,
+            "P1": 70.68298973816404,
+            "N1": 70.68298973816404,
+            "EPL": 57.911999997719626,
+            "EPD": 20.000749574167443,
+            "XPL": -114.64441695254197,
+            "XPD": 22.928885438346853,
+            "FNO": 5,
+            "invariant": -2.4933734733468826
+        }),
+        (ReverseTelephoto(), {
+            "f1": -2.0052402707991126,
+            "f2": 2.005240270799113,
+            "F1": 0.04272711870891329,
+            "F2": 0.023698437250481586,
+            "P1": 2.0479673895080257,
+            "N1": 2.047967389508026,
+            "EPL": 1.3489734720774773,
+            "EPD": 0.3,
+            "XPL": -3.0545788978518327,
+            "XPD": 0.46053493637581644,
+            "FNO": 6.6841342359970435,
+            "invariant": -0.08660254037844384
+        }),
+        (ObjectiveUS008879901(), {
+            "f1": -35.97568215109414,
+            "f2": 35.97568215109414,
+            "F1": 29.11251211499461,
+            "F2": 0.3439121552801502,
+            "P1": 65.08819426608875,
+            "N1": 65.08819426608875,
+            "EPL": 34.093405743879785,
+            "EPD": 17.98784107554707,
+            "XPL": -259.49895996127134,
+            "XPD": 129.9214360582758,
+            "FNO": 2.0,
+            "invariant": -1.7189360314692992
+        }),
+        (TelescopeObjective48Inch(), {
+            "f1": -47.853468289530184,
+            "f2": 47.85346828953009,
+            "F1": -47.860903724382666,
+            "F2": -0.13665267213750804,
+            "P1": -0.0074354348524821035,
+            "N1": -0.007435434852574474,
+            "EPL": 0.5182309943090306,
+            "EPD": 7.975578048255016,
+            "XPL": -47.47016619304641,
+            "XPD": 7.888918920151483,
+            "FNO": 6,
+            "invariant": -0.7031547983267595
+        }),
+        (HeliarLens(), {
+            "f1": -10.006529335414514,
+            "f2": 10.006529335414516,
+            "F1": -7.951972858123898,
+            "F2": 0.014434232832484266,
+            "P1": 2.0545564772906166,
+            "N1": 2.0545564772906184,
+            "EPL": 2.396910654734009,
+            "EPD": 2.001305867082903,
+            "XPL": -9.661066435051593,
+            "XPD": 1.935100133576815,
+            "FNO": 5,
+            "invariant": -0.17644211050843242
+        }),
+        (TessarLens(), {
+            "f1": -3.9977777470211935,
+            "f2": 3.9977777470211935,
+            "F1": -3.4047630367687556,
+            "F2": 0.016255381553407968,
+            "P1": 0.5930147102524379,
+            "N1": 0.5930147102524379,
+            "EPL": 0.6587555774251294,
+            "EPD": 0.8883950548935986,
+            "XPL": -3.9168450744779424,
+            "XPD": 0.8740223235625226,
+            "FNO": 4.5,
+            "invariant": -0.1660786501773892
+        }),
+        (LensWithFieldCorrector(), {
+            "f1": -127.05805143206146,
+            "f2": 127.0580514320616,
+            "F1": -130.3393318072395,
+            "F2": 0.30352823524105277,
+            "P1": -3.281280375178028,
+            "N1": -3.281280375177886,
+            "EPL": 25.92650579168234,
+            "EPD": 36.30230040916046,
+            "XPL": -103.0059902223222,
+            "XPD": 29.51700527358951,
+            "FNO": 3.5,
+            "invariant": -3.0863326731201335
+        }),
+        (PetzvalLens(), {
+            "f1": -50.58453173454361,
+            "f2": 50.584531734543596,
+            "F1": -56.92942085724116,
+            "F2": 0.23541781883782253,
+            "P1": -6.344889122697552,
+            "N1": -6.344889122697566,
+            "EPL": 16.62941090841462,
+            "EPD": 36.13180838181685,
+            "XPL": -34.550273978917666,
+            "XPD": 24.846922712682495,
+            "FNO": 1.4,
+            "invariant": -2.218214113394635
+        }),
+        (Telephoto(), {
+            "f1": -127.01660113786153,
+            "f2": 127.01660113786154,
+            "F1": -165.12532147743212,
+            "F2": 0.42421943964553055,
+            "P1": -38.108720339570596,
+            "N1": -38.10872033957058,
+            "EPL": 7.783812370889942,
+            "EPD": 22.681535917475276,
+            "XPL": -92.880377058807,
+            "XPD": 16.661535089009377,
+            "FNO": 5.6,
+            "invariant": -1.999683373079509
+        })
+    }
+
+
+def test_paraxial_init():
+    optic = Optic()  # Create a dummy Optic object
+    paraxial = Paraxial(optic)
+
+    assert paraxial.optic == optic
+    assert paraxial.surfaces == optic.surface_group
+
+
+@pytest.mark.parametrize('optic_instance,values', get_optic_data())
+def test_calculate_first_order_properties(optic_instance, values):
+    assert optic_instance.paraxial == values['f1']
+    assert optic_instance.paraxial == values['f2']
+    assert optic_instance.paraxial == values['F1']
+    assert optic_instance.paraxial == values['F2']
+    assert optic_instance.paraxial == values['P1']
+    assert optic_instance.paraxial == values['N1']
