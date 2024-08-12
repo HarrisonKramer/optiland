855536a5b673a63fb9dbb0d0d6003dcef7703b2c
Kramer <kdanielharrison@gmail.com>
2024-08-12 22:33:17 +0200

    add SolveFactory

--- optiland/solves.py
+++ optiland/solves.py
@@ -38,3 +38,43 @@ class MarginalRayHeightSolve(BaseSolve):
         # shift current surface and all subsequent surfaces
         for surface in self.optic.surface_group.surfaces[self.surface_idx:]:
             surface.geometry.cs.z -= offset
+
+
+class SolveFactory:
+    """
+    Factory class for creating solves.
+
+    Attributes:
+        _solve_map (dict): A dictionary mapping solve types to solve classes.
+
+    Methods:
+        create_solve(solve_type, *args, **kwargs): Creates a solve instance
+            based on the given solve type.
+    """
+    _solve_map = {
+        'marginal_ray_height': MarginalRayHeightSolve
+    }
+
+    @staticmethod
+    def create_solve(optic, solve_type, surface_idx, *args, **kwargs):
+        """
+        Creates a solve instance based on the given solve type.
+
+        Args:
+            optic (Optic): The optic object.
+            solve_type (str): The type of solve to create.
+            surface_idx (int): The index of the surface.
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            solve_instance: An instance of the solve class corresponding to
+                the given solve type.
+
+        Raises:
+            ValueError: If the solve type is invalid.
+        """
+        solve_class = SolveFactory._solve_map.get(solve_type)
+        if solve_class is None:
+            raise ValueError(f'Invalid solve type: {solve_type}')
+        return solve_class(optic, surface_idx, *args, **kwargs)
