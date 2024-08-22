d99484cbcf5aab524ff35f2374003b290e63533a
Kramer <kdanielharrison@gmail.com>
2024-08-22 15:45:16 +0200

    add aperture, field, wavelength configurations to ZemaxToOpticConverter

--- optiland/file_handler.py
+++ optiland/file_handler.py
@@ -5,6 +5,7 @@ This module provides classes for parsing files, such as Zemax lens data files.
 Kramer Harrison, 2024
 """
 import numpy as np
+from optiland.optic import Optic
 
 
 class ZemaxFileReader:
@@ -331,7 +332,50 @@ class ZemaxFileReader:
 class ZemaxToOpticConverter:
 
     def __init__(self, zemax_data):
-        self.zemax_data = zemax_data
+        self.data = zemax_data
+        self.optic = None
 
     def convert(self):
+        """
+        Converts the configuration of the file handler into an Optic object.
+
+        Returns:
+            Optic: The Optic object based on the Zemax data.
+        """
+        self.optic = Optic()
+        self._configure_surfaces()
+        self._configure_aperture()
+        self._configure_fields()
+        self._configure_wavelengths()
+        return self.optic
+
+    def _configure_surfaces(self):
         pass
+
+    def _configure_surface(self):
+        pass
+
+    def _configure_surface_type(self):
+        pass
+
+    def _configure_aperture(self):
+        """
+        Configures the aperture for the optic.
+        """
+        aperture_data = self.data['aperture']
+        ap_type, value = next(iter(aperture_data.items()))
+        self.optic.set_aperture(aperture_type=ap_type, value=value)
+
+    def _configure_fields(self):
+        """
+        Configure the fields for the optic.
+        """
+        self.optic.set_field_type(field_type=self.data['fields']['type'])
+        for fx, fy in zip(self.data['fields']['x'], self.data['fields']['y']):
+            self.optic.add_field(x=fx, y=fy)
+
+    def _configure_wavelengths(self):
+        primary_idx = self.data['wavelengths']['primary_index']
+        for idx, value in enumerate(self.data['wavelengths']['data']):
+            self.optic.add_wavelength(value=value,
+                                      is_primary=(idx == primary_idx))
