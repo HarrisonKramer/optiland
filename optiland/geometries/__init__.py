58c12a60f7d24741c969e915affa43dc17e7b588
Kramer <kdanielharrison@gmail.com>
2024-09-15 16:05:44 +0200

    refactor: Move each geometry class to its own module

---
+++ optiland/geometries/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+
+from .base import BaseGeometry
+from .plane import Plane
+from .standard import StandardGeometry
+from .newton_raphson import NewtonRaphsonGeometry
+from .even_asphere import EvenAsphere
+from .polynomial import PolynomialGeometry
+from .chebyshev import ChebyshevPolynomialGeometry
