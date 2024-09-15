a6e38b5d2cf0664d06fe8da6a75d4b4afde29d88
Kramer <kdanielharrison@gmail.com>
2024-09-15 17:04:50 +0200

    refactor: Move ray classes to their own module

---
+++ optiland/rays/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+
+from .base import BaseRays
+from .real_rays import RealRays
+from .paraxial_rays import ParaxialRays
+from .polarized_rays import PolarizedRays
+from .ray_generator import RayGenerator
+from .polarization_state import PolarizationState, create_polarization
