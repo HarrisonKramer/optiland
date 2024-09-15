4fdbf69592533e09c029d1467506247943b563ca
Kramer <kdanielharrison@gmail.com>
2024-09-15 14:30:55 +0200

    feat: Add analysis module with various classes
    This commit adds a new module called "analysis" to the "optiland" package. The module includes several classes for different types of analysis, such as SpotDiagram, EncircledEnergy, RayFan, YYbar, Distortion, GridDistortion, and FieldCurvature.
    
    The classes in the analysis module will be used for analyzing optical systems and performing various calculations.

---
+++ optiland/analysis/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+
+from .spot_diagram import SpotDiagram
+from .encircled_energy import EncircledEnergy
+from .ray_fan import RayFan
+from .y_ybar import YYbar
+from .distortion import Distortion
+from .grid_distortion import GridDistortion
+from .field_curvature import FieldCurvature
