9003dd3da9c98a0200c6492b28f1c6ec3ebb1503
Kramer <kdanielharrison@gmail.com>
2024-09-16 21:12:05 +0200

    feat: Add Tolerancing class for optic tolerancing problem

---
+++ optiland/tolerancing/core.py
@@ -0,0 +1,38 @@
+from copy import deepcopy
+from optiland.tolerancing.perturbation import OpticPerturbation
+from optiland.tolerancing.compensator import CompensatorOptimizer
+from optiland.optimization.operand import Operand
+
+
+class Tolerancing:
+
+    def __init__(self, optic):
+        self.optic = optic
+        self._optic_nominal = deepcopy(optic)
+        self.operands = []
+        self.perturbations = []
+        self._compensator = CompensatorOptimizer()
+
+    def add_operand(self, operand_type, input_data={}):
+        """Add an operand to the tolerancing problem."""
+        self.operands.append(Operand(operand_type, 0.0, 1.0, input_data))
+
+    def add_perturbation(self, variable_type, perturbation, **kwargs):
+        perturbation = OpticPerturbation(self.optic, variable_type,
+                                         perturbation, **kwargs)
+        self.perturbations.append(perturbation)
+
+    def add_compensator(self, variable_type, **kwargs):
+        self._compensator.add_variable(self.optic, variable_type, **kwargs)
+
+    def apply_compensators(self):
+        if self._compensator.has_variables:
+            # add operands to the optimization problem (for compensation)
+            self._compensator.operands = self.operands
+
+            # Run optimizer for compensating the perturbations
+            self._compensator.run()
+
+    def reset(self):
+        """Reset the optic to its nominal state."""
+        self.optic = deepcopy(self._optic_nominal)
