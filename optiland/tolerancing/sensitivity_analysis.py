8aa2c782a361049a496e6467ef1bd93344a45f51
Kramer <kdanielharrison@gmail.com>
2024-09-16 21:54:26 +0200

    refactor: Refactor SensitivityAnalysis to use pandas dataframe

--- optiland/tolerancing/sensitivity_analysis.py
+++ optiland/tolerancing/sensitivity_analysis.py
@@ -1,25 +1,21 @@
-from dataclasses import dataclass
-from typing import List
+import pandas as pd
 import matplotlib.pyplot as plt
 from optiland.tolerancing.core import Tolerancing
 from optiland.tolerancing.perturbation import RangePerturbation
-
-
-@dataclass
-class IterationResult:
-    perturbation_id: int
-    perturbation_value: float
-    operand_values: List[float]
 
 
 class SensitivityAnalysis:
 
     def __init__(self, tolerancing: Tolerancing):
         self.tolerancing = tolerancing
-        self._results = []
+        self.operand_names = [f'operand_{i}'
+                              for i in range(len(self.tolerancing.operands))]
+        columns = ['perturbation_id', 'perturbation_value'] + \
+            self.operand_names
+        self._results = pd.DataFrame(columns=columns)
 
     def run(self):
-        self._results = []
+        results = []
 
         for idx, perturbation in enumerate(self.tolerancing.perturbations):
             if not isinstance(perturbation.perturbation, RangePerturbation):
@@ -32,30 +28,24 @@ class SensitivityAnalysis:
 
                 # apply perturbation
                 perturbation.apply()
-                perturbation_value = perturbation.perturbation.sample()
 
                 # apply compensators
                 self.tolerancing.apply_compensators()
 
-                # evaluate operands & record results
+                # evaluate operands
                 operand_values = [operand.value
                                   for operand in self.tolerancing.operands]
-                self._results.append(
-                    IterationResult(idx, perturbation_value, operand_values)
-                    )
+
+                # save results
+                result = {
+                    'perturbation_id': idx,
+                    'perturbation_value': perturbation.value,
+                }
+                result.update({f'operand_{i}': value
+                               for i, value in enumerate(operand_values)})
+                results.append(result)
+
+        self._results = pd.DataFrame(results)
 
     def view(self):
-        unique_ids = set(result.perturbation_id for result in self._results)
-        for perturbation_id in unique_ids:
-            perturbation_values = []
-            operand_values = []
-            for result in self._results:
-                if result.perturbation_id == perturbation_id:
-                    perturbation_values.append(result.perturbation_value)
-                    operand_values.append(result.operand_values)
-            plt.scatter(perturbation_values, operand_values,
-                        label=f'Perturbation ID: {perturbation_id}', s=4)
-        plt.xlabel('Perturbation Value')
-        plt.ylabel('Operand Value')
-        plt.legend()
-        plt.show()
+        pass
