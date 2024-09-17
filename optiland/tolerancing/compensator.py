e7501bbaa9c183034659cbaf34aa5f8ffbc6a06f
Kramer <kdanielharrison@gmail.com>
2024-09-17 17:03:22 +0200

    docs: add docs for CompensatorOptimizer class and methods

--- optiland/tolerancing/compensator.py
+++ optiland/tolerancing/compensator.py
@@ -7,6 +7,24 @@ from optiland.optimization import (
 
 
 class CompensatorOptimizer(OptimizationProblem):
+    """
+    A class representing a compensator for a tolerancing problem. This class
+    optimizes a set of variables to compensate for perturbations in an optical
+    system.
+
+    Args:
+        optimizer (str): The type of optimizer to use. Default is 'generic'.
+            Other options include 'least_squares', 'dual_annealing', and
+            'differential_evolution'.
+        **kwargs: Additional keyword arguments to be passed to the optimizer.
+
+    Attributes:
+        optimizer (str): The type of optimizer being used.
+        kwargs (dict): Additional keyword arguments passed to the optimizer.
+        _optimizer_map (dict): A mapping of optimizer types to their
+            respective classes.
+    """
+
     def __init__(self, optimizer='generic', **kwargs):
         super().__init__()
         self.optimizer = optimizer
@@ -21,8 +39,21 @@ class CompensatorOptimizer(OptimizationProblem):
 
     @property
     def has_variables(self):
+        """
+        Check if the optimizer has variables. If no variables are present,
+        then the optimizer will not run.
+
+        Returns:
+            bool: True if the optimizer has variables, False otherwise.
+        """
         return bool(self.variables)
 
     def run(self):
+        """
+        Run the optimizer for compensation.
+
+        Returns:
+            scipy.optimize.OptimizeResult: The result of the optimizer run.
+        """
         optimizer = self._optimizer_map[self.optimizer](**self.kwargs)
         return optimizer.run(self)
