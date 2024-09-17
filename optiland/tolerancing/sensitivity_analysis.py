f8501993aa899ae2c03b1ab7a39d01b2d2dbce67
Kramer <kdanielharrison@gmail.com>
2024-09-17 18:10:14 +0200

    refactor: Refactor SensitivityAnalysis class in sensitivity_analysis.py
    Refactor the SensitivityAnalysis class in sensitivity_analysis.py to improve code readability and maintainability. The changes include:
    - Adding a docstring to the class to provide a brief description of its purpose and usage.
    - Updating the attribute names to be more descriptive.
    - Adding a validation method to ensure the tolerancing system is properly set up before performing sensitivity analysis.

--- optiland/tolerancing/sensitivity_analysis.py
+++ optiland/tolerancing/sensitivity_analysis.py
@@ -5,19 +5,55 @@ from optiland.tolerancing.perturbation import RangeSampler
 
 
 class SensitivityAnalysis:
+    """
+    Class for performing sensitivity analysis on a tolerancing system.
+
+    Args:
+        tolerancing (Tolerancing): The tolerancing system to perform
+            sensitivity analysis on.
+
+    Attributes:
+        tolerancing (Tolerancing): The tolerancing system to perform
+            sensitivity analysis on.
+        operand_names (list): List of operand names in the tolerancing system.
+        _results (pd.DataFrame): DataFrame to store the sensitivity analysis
+            results.
+
+    Methods:
+        run(): Runs the sensitivity analysis.
+        get_results(): Returns the sensitivity analysis results.
+        view(figsize=(2.2, 3), sharex='col', sharey='row'): Visualizes the
+            sensitivity analysis results.
+    """
 
     def __init__(self, tolerancing: Tolerancing):
         self.tolerancing = tolerancing
-        self.operand_names = [f'operand_{i}'
-                              for i in range(len(self.tolerancing.operands))]
-        columns = ['perturbation_id', 'perturbation_value'] + \
+        self.operand_names = [
+            f'{i}: {operand}' for i, operand in enumerate(tolerancing.operands)
+            ]
+        columns = ['perturbation_type', 'perturbation_value'] + \
             self.operand_names
         self._results = pd.DataFrame(columns=columns)
 
+        self._validate()
+
     def run(self):
+        """
+        Run the sensitivity analysis.
+
+        This method performs a sensitivity analysis by iterating over the
+        perturbations defined in the tolerancing object. For each
+        perturbation, it applies the perturbation, applies compensators,
+        evaluates operands, and saves the results. The results are stored in a
+        pandas DataFrame in the _results attribute.
+
+        Raises:
+            ValueError: If a perturbation sampler other than RangeSampler is
+                used.
+        """
         results = []
 
-        for idx, perturbation in enumerate(self.tolerancing.perturbations):
+        for perturbation in self.tolerancing.perturbations:
             if not isinstance(perturbation.sampler, RangeSampler):
                 raise ValueError('Only range samplers are supported.')
 
@@ -33,25 +69,93 @@ class SensitivityAnalysis:
                 self.tolerancing.apply_compensators()
 
                 # evaluate operands
-                operand_values = self.tolerancing.evaluate()
+                values = self.tolerancing.evaluate()
 
                 # save results
                 result = {
-                    'perturbation_id': idx,
+                    'perturbation_type': perturbation.variable,
                     'perturbation_value': perturbation.value,
                 }
-                result.update({f'operand_{i}': value
-                               for i, value in enumerate(operand_values)})
+                result.update({
+                    f'{name}': value
+                    for name, value in zip(self.operand_names, values)
+                })
                 results.append(result)
 
         self._results = pd.DataFrame(results)
 
-    def view(self):
+    def get_results(self):
+        """
+        Returns the results of the sensitivity analysis.
+
+        Returns:
+            pd.DataFrame: The results of the sensitivity analysis.
+        """
+        return self._results
+
+    def view(self, figsize=(2.2, 3), sharex='col', sharey='row'):
+        """
+        Visualizes the sensitivity analysis results.
+
+        Args:
+            figsize (tuple, optional): The size of the figure in inches for
+                each subplot. Default is (2.2, 3).
+            sharex (str, optional): Specifies how the x-axis is shared among
+                subplots. Default is 'col'.
+            sharey (str, optional): Specifies how the y-axis is shared among
+                subplots. Default is 'row'.
+        """
         df = self._results
-        unique_ids = df['perturbation_id'].unique()
-        for idx in unique_ids:
-            for name in self.operand_names:
-                x = df.loc[df.perturbation_id == idx, 'perturbation_value']
-                y = df.loc[df.perturbation_id == idx, name]
-                plt.plot(x, y)
-                plt.show()
+        unique_types = df['perturbation_type'].unique()
+
+        m = len(self.operand_names)
+        n = len(unique_types)
+
+        size_x = m * figsize[0]
+        size_y = n * figsize[1]
+
+        fig, axes = plt.subplots(m, n, figsize=(size_y, size_x), sharex=sharex,
+                                 sharey=sharey)
+
+        for i, name in enumerate(self.operand_names):
+            for j, pert_type in enumerate(unique_types):
+                x = df.loc[df.perturbation_type == pert_type,
+                           'perturbation_value']
+                y = df.loc[df.perturbation_type == pert_type, name]
+                axes[i, j].plot(x, y, color=f'C{i}', linewidth=2)
+                axes[i, j].grid()
+
+                if j == 0:
+                    axes[i, j].set_ylabel(name)
+
+                if i == m - 1:
+                    axes[i, j].set_xlabel(pert_type)
+
+        plt.tight_layout()
+        plt.show()
+
+    def _validate(self):
+        """
+        Validates the tolerancing system before performing sensitivity
+        analysis.
+
+        Raises:
+            ValueError: If no operands are found in the tolerancing system.
+            ValueError: If no perturbations are found in the tolerancing
+                system.
+            ValueError: If the number of operands exceeds 6.
+            ValueError: If the number of perturbations exceeds 6.
+        """
+        if not self.tolerancing.operands:
+            raise ValueError('No operands found in the tolerancing system.')
+
+        if not self.tolerancing.perturbations:
+            raise ValueError('No perturbations found in the tolerancing '
+                             'system.')
+
+        if len(self.tolerancing.operands) > 6:
+            raise ValueError('Sensitivity analysis is limited to 6 operands.')
+
+        if len(self.tolerancing.perturbations) > 6:
+            raise ValueError('Sensitivity analysis is limited to 6 '
+                             'perturbations.')
