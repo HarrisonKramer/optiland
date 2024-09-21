ba93201d84a7e57a05963de83f1954bc721c3cc6
Kramer <kdanielharrison@gmail.com>
2024-09-21 10:41:21 +0200

    chore: move variables into variable module

---
+++ optiland/optimization/variable/__init__.py
@@ -0,0 +1,13 @@
+# flake8: noqa
+
+from .base import VariableBehavior
+from .radius import RadiusVariable
+from .conic import ConicVariable
+from .thickness import ThicknessVariable
+from .tilt import TiltVariable
+from .decenter import DecenterVariable
+from .index import IndexVariable
+from .asphere_coeff import AsphereCoeffVariable
+from .polynomial_coeff import PolynomialCoeffVariable
+from .chebyshev_coeff import ChebyshevCoeffVariable
+from .variable import Variable
