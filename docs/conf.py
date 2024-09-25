d4334c619ff27d8cd4574653fcab4d49825c8a2f
Kramer <kdanielharrison@gmail.com>
2024-09-25 22:26:14 +0200

    docs: Add Sphinx gallery support to Optiland documentation

--- docs/conf.py
+++ docs/conf.py
@@ -12,10 +12,16 @@ release = '0.1.3'
 extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.napoleon',
               'sphinx.ext.viewcode',
-              "nbsphinx"]
+              'nbsphinx',
+              'sphinx_gallery.gen_gallery']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+sphinx_gallery_conf = {
+     'examples_dirs': 'examples',   # path to your example scripts
+     'gallery_dirs': 'auto_examples',  # path to where to save gallery generated output
+}
