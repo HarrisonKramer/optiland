5d8560c388180e120626d38f3d4120a34430add9
Kramer <kdanielharrison@gmail.com>
2024-10-14 21:33:01 +0200

    feat: add transform_3d function in utils.py

--- optiland/visualization/utils.py
+++ optiland/visualization/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import vtk
 from optiland.rays import RealRays
 
 
@@ -25,3 +26,72 @@ def transform(x, y, z, surface, is_global=True):
         surface.geometry.globalize(points)
 
     return points.x, points.y, points.z
+
+
+def transform_3d(actor, surface):
+    """
+    Applies the effective rotation and translation of a surface to an actor.
+
+    Args:
+        actor: The actor to be transformed. This is typically an instance of a
+            VTK actor.
+        surface: The surface whose transformation will be applied to the actor.
+
+    Returns:
+        The transformed actor with the applied rotation and translation.
+    """
+    # Get the effective rotation and translation of the surface
+    cs = surface.geometry.cs
+    rx, ry, rz = cs.get_effective_rotation_euler()
+    translation, _ = cs.get_effective_transform()
+    dx, dy, dz = translation
+
+    # Apply the rotation and translation to the actor
+    actor.RotateZ(np.degrees(rz))
+    actor.RotateY(np.degrees(ry))
+    actor.RotateX(np.degrees(rx))
+    actor.SetPosition(dx, dy, dz)
+
+    return actor
+
+
+def revolve_contour(x, y, z):
+    """
+    Revolves a contour defined by the input points (x, y, z) around the z-axis
+    to create a 3D surface and returns the corresponding VTK actor.
+
+    Args:
+        x (list of float): List of x-coordinates of the contour points.
+        y (list of float): List of y-coordinates of the contour points.
+        z (list of float): List of z-coordinates of the contour points.
+
+    Returns:
+        vtk.vtkActor: VTK actor representing the revolved 3D surface.
+    """
+    pts = [(xi, yi, zi) for xi, yi, zi in zip(x, y, z)]
+
+    points = vtk.vtkPoints()
+    lines = vtk.vtkCellArray()
+    for pt in pts:
+        pt_id = points.InsertNextPoint(pt)
+        if pt_id < len(pts) - 1:
+            line = vtk.vtkLine()
+            line.GetPointIds().SetId(0, pt_id)
+            line.GetPointIds().SetId(1, pt_id + 1)
+            lines.InsertNextCell(line)
+
+    poly_data = vtk.vtkPolyData()
+    poly_data.SetPoints(points)
+    poly_data.SetLines(lines)
+
+    revolution = vtk.vtkRotationalExtrusionFilter()
+    revolution.SetInputData(poly_data)
+    revolution.SetResolution(256)
+
+    surface_mapper = vtk.vtkPolyDataMapper()
+    surface_mapper.SetInputConnection(revolution.GetOutputPort())
+
+    actor = vtk.vtkActor()
+    actor.SetMapper(surface_mapper)
+
+    return actor
