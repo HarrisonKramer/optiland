25ca2ee6f67a25e70496cc6ff22bb66593a60288
Kramer <kdanielharrison@gmail.com>
2024-10-16 20:36:42 +0200

    chore: Update Optiland version to 0.1.5 and enable autosummary generation
    Update the Optiland version in the documentation configuration file to 0.1.5. Additionally, enable the autosummary generation feature to automatically generate summaries for the documented modules.

--- docs/conf.py
+++ docs/conf.py
@@ -7,15 +7,19 @@ sys.path.insert(0, os.path.abspath('..'))
 project = 'Optiland'
 copyright = '2024, Kramer Harrison'
 author = 'Kramer Harrison'
-release = '0.1.3'
+release = '0.1.5'
 
 extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.autosummary',
               'sphinx.ext.napoleon',
               'sphinx.ext.viewcode',
               'nbsphinx',
               'sphinx_gallery.gen_gallery']
 
+autosummary_generate = True  # Automatically generate summaries
+
 templates_path = ['_templates']
+modindex_common_prefix = ['optiland.']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'sphinx_rtd_theme'
