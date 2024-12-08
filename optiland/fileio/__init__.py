f899856b00d751065db3f2b12569263ced60ac3f
Kramer <kdanielharrison@gmail.com>
2024-12-08 10:39:09 -0600

    chore: import load_zemax_file in __init__.py for improved accessibility

--- optiland/fileio/__init__.py
+++ optiland/fileio/__init__.py
@@ -1,4 +1,4 @@
 # flake8: noqa
 
-from .zemax_handler import ZemaxFileReader
+from .zemax_handler import ZemaxFileReader, load_zemax_file
 from .optiland_handler import load_optiland_file, save_optiland_file
