8d286e73036b779ae48a92e8034fe397600f0970
Kramer <kdanielharrison@gmail.com>
2024-12-29 20:03:00 -0600

    feat: Import OperandManager in operand module

--- optiland/optimization/operand/__init__.py
+++ optiland/optimization/operand/__init__.py
@@ -4,3 +4,4 @@ from .paraxial import ParaxialOperand
 from .aberration import AberrationOperand
 from .ray import RayOperand
 from .operand import Operand, operand_registry
+from .operand_manager import OperandManager
