c60b4097fb0add3e2219c73e07a5272d2e6bc874
Kramer <kdanielharrison@gmail.com>
2025-01-25 13:02:09 +0100

    docs: update output table format in optimization.py for improved readability

--- optiland/optimization/optimization.py
+++ optiland/optimization/optimization.py
@@ -95,7 +95,7 @@ class OptimizationProblem:
         funs = self.fun_array()
         df['Contribution (%)'] = funs / np.sum(funs) * 100
 
-        print(df.to_markdown(headers='keys', tablefmt='psql'))
+        print(df.to_markdown(headers='keys', tablefmt='fancy_outline'))
 
     def variable_info(self):
         """Print information about the variables in the merit function."""
@@ -107,7 +107,7 @@ class OptimizationProblem:
                 'Max. Bound': [var.max_val for var in self.variables]}
 
         df = pd.DataFrame(data)
-        print(df.to_markdown(headers='keys', tablefmt='psql'))
+        print(df.to_markdown(headers='keys', tablefmt='fancy_outline'))
 
     def merit_info(self):
         """Print information about the merit function."""
@@ -122,7 +122,7 @@ class OptimizationProblem:
         data = {'Merit Function Value': [self.sum_squared()],
                 'Improvement (%)': improve_percent}
         df = pd.DataFrame(data)
-        print(df.to_markdown(headers='keys', tablefmt='psql'))
+        print(df.to_markdown(headers='keys', tablefmt='fancy_outline'))
 
     def info(self):
         """Print information about the optimization problem."""
