d02066e98673613b3569153631c2d736d171dc7b
Kramer <kdanielharrison@gmail.com>
2025-01-28 20:00:28 +0100

    feat: add reference option for ray plotting in draw and draw3D methods

--- optiland/optic.py
+++ optiland/optic.py
@@ -293,7 +293,8 @@ class Optic:
                 surface.aperture.scale(scale_factor)
 
     def draw(self, fields='all', wavelengths='primary', num_rays=3,
-             figsize=(10, 4), xlim=None, ylim=None):
+             distribution='line_y', figsize=(10, 4), xlim=None, ylim=None,
+             reference=None):
         """
         Draw a 2D representation of the optical system.
 
@@ -304,19 +305,24 @@ class Optic:
                 displayed. Defaults to 'primary'.
             num_rays (int, optional): The number of rays to be traced for each
                 field and wavelength. Defaults to 3.
+            distribution (str, optional): The distribution of the rays.
+                Defaults to 'line_y'.
             figsize (tuple, optional): The size of the figure. Defaults to
                 (10, 4).
             xlim (tuple, optional): The x-axis limits of the plot. Defaults to
                 None.
             ylim (tuple, optional): The y-axis limits of the plot. Defaults to
                 None.
+            reference (str, optional): The reference rays to plot. Options
+                include "chief" and "marginal". Defaults to None.
         """
         viewer = OpticViewer(self)
-        viewer.view(fields, wavelengths, num_rays, distribution='line_y',
-                    figsize=figsize, xlim=xlim, ylim=ylim)
+        viewer.view(fields, wavelengths, num_rays, distribution=distribution,
+                    figsize=figsize, xlim=xlim, ylim=ylim, reference=reference)
 
     def draw3D(self, fields='all', wavelengths='primary', num_rays=24,
-               figsize=(1200, 800), dark_mode=False):
+               distribution='ring', figsize=(1200, 800), dark_mode=False,
+               reference=None):
         """
         Draw a 3D representation of the optical system.
 
@@ -327,14 +333,19 @@ class Optic:
                 displayed. Defaults to 'primary'.
             num_rays (int, optional): The number of rays to be traced for each
                 field and wavelength. Defaults to 2.
+            distribution (str, optional): The distribution of the rays.
+                Defaults to 'ring'.
             figsize (tuple, optional): The size of the figure. Defaults to
                 (1200, 800).
             dark_mode (bool, optional): Whether to use dark mode. Defaults to
                 False.
+            reference (str, optional): The reference rays to plot. Options
+                include "chief" and "marginal". Defaults to None.
         """
         viewer = OpticViewer3D(self)
         viewer.view(fields, wavelengths, num_rays,
-                    distribution='ring', figsize=figsize, dark_mode=dark_mode)
+                    distribution=distribution, figsize=figsize,
+                    dark_mode=dark_mode, reference=reference)
 
     def info(self):
         """Display the optical system information."""
