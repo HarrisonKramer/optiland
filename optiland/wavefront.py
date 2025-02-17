fix: correct tilt correction calculation in Wavefront class

optiland/wavefront.py
@@ -199,7 +199,7 @@ class Wavefront:
             if y is None:
                 y = self.distribution.y
             EPD = self.optic.paraxial.EPD()
-            tilt_correction = ((1 - x) * np.sin(np.radians(x_tilt)) * EPD / 2 +
+            tilt_correction = ((x - 1) * np.sin(np.radians(x_tilt)) * EPD / 2 +
                                (1 - y) * np.sin(np.radians(y_tilt)) * EPD / 2)
         return opd - tilt_correction
 