fix: correct sign errors in grid distortion and ray generation calculations

optiland/analysis/grid_distortion.py
@@ -116,7 +116,7 @@ class GridDistortion:
                                 (self.num_points, self.num_points))
 
         # optical system flips x, so must correct this
-        data['xp'] = np.flip(xp)
+        data['xp'] = xp
         data['yp'] = yp
 
         # Find max distortion

tests/test_operand.py
@@ -144,7 +144,7 @@ class TestRayOperand:
         data = {'optic': hubble, 'surface_number': -1, 'Hx': 1.0, 'Hy': 0.0,
                 'Px': 0.0, 'Py': 0.0, 'wavelength': 0.55}
         assert np.isclose(operand.RayOperand.x_intercept(**data),
-                          -150.42338010762842)
+                          150.42338010762842)
 
     def test_y_intercept(self, hubble):
         data = {'optic': hubble, 'surface_number': -1, 'Hx': 0.0, 'Hy': 1.0,
@@ -162,7 +162,7 @@ class TestRayOperand:
         data = {'optic': hubble, 'surface_number': -1, 'Hx': 1.0, 'Hy': 0.0,
                 'Px': 0.0, 'Py': 0.0, 'wavelength': 0.55}
         assert np.isclose(operand.RayOperand.x_intercept_lcs(**data),
-                          -150.42338010762842)
+                          150.42338010762842)
 
     def test_y_intercept_lcs(self, hubble):
         data = {'optic': hubble, 'surface_number': -1, 'Hx': 0.0, 'Hy': 1.0,

tests/test_wavefront.py
@@ -216,7 +216,7 @@ class TestZernikeOPD:
         c = zernike_opd.zernike.coeffs
         assert np.isclose(c[0], 0.8430890395012354)
         assert np.isclose(c[1], 6.863699034904449e-13)
-        assert np.isclose(c[2], -0.14504379704525455)
+        assert np.isclose(c[2], 0.14504379704525455)
         assert np.isclose(c[6], -1.160298338689596e-13)
         assert np.isclose(c[24], -0.0007283668376039182)
 