da690fe852b04d93611020e77a3aedf4debcc974
Kramer <kdanielharrison@gmail.com>
2025-02-20 17:35:11 +0100

    feat: add RectangularAperture class for ray clipping and scaling

--- optiland/physical_apertures.py
+++ optiland/physical_apertures.py
@@ -197,3 +197,73 @@ class OffsetRadialAperture(RadialAperture):
         """
         return cls(data['r_max'], data['r_min'],
                    data['offset_x'], data['offset_y'])
+
+
+class RectangularAperture(BaseAperture):
+    """
+    Represents a rectangular aperture that clips rays based on their position.
+
+    Attributes:
+        x_min (float): The minimum x-coordinate allowed for the rays.
+        x_max (float): The maximum x-coordinate allowed for the rays.
+        y_min (float): The minimum y-coordinate allowed for the rays.
+        y_max (float): The maximum y-coordinate allowed for the rays.
+    """
+
+    def __init__(self, x_min, x_max, y_min, y_max):
+        super().__init__()
+        self.x_min = x_min
+        self.x_max = x_max
+        self.y_min = y_min
+        self.y_max = y_max
+
+    def clip(self, rays):
+        """
+        Clips the given rays based on their position.
+
+        Args:
+            rays (Rays): The rays to be clipped.
+        """
+        condition = (rays.x < self.x_min) | (rays.x > self.x_max) | \
+                    (rays.y < self.y_min) | (rays.y > self.y_max)
+        rays.clip(condition)
+
+    def scale(self, scale_factor):
+        """
+        Scales the aperture by the given factor.
+
+        Args:
+            scale_factor (float): The factor by which to scale the aperture.
+        """
+        self.x_min *= scale_factor
+        self.x_max *= scale_factor
+        self.y_min *= scale_factor
+        self.y_max *= scale_factor
+
+    def to_dict(self):
+        """
+        Convert the aperture to a dictionary.
+
+        Returns:
+            dict: The dictionary representation of the aperture.
+        """
+        aperture_dict = super().to_dict()
+        aperture_dict['x_min'] = self.x_min
+        aperture_dict['x_max'] = self.x_max
+        aperture_dict['y_min'] = self.y_min
+        aperture_dict['y_max'] = self.y_max
+        return aperture_dict
+
+    @classmethod
+    def from_dict(cls, data):
+        """
+        Create an aperture from a dictionary representation.
+
+        Args:
+            data (dict): The dictionary representation of the aperture.
+
+        Returns:
+            RectangularAperture: The aperture object.
+        """
+        return cls(data['x_min'], data['x_max'],
+                   data['y_min'], data['y_max'])
