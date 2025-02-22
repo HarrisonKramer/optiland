ec232dcc1796eba34524b31cfce27c2baa265239
Kramer <kdanielharrison@gmail.com>
2025-02-22 08:44:00 +0100

    feat: enhance tests for Physical Apertures with additional boolean aperture classes

--- tests/test_physical_apertures.py
+++ tests/test_physical_apertures.py
@@ -1,11 +1,20 @@
 import numpy as np
-from optiland import physical_apertures
+from optiland.physical_apertures import (
+    RadialAperture,
+    OffsetRadialAperture,
+    UnionAperture,
+    IntersectionAperture,
+    DifferenceAperture,
+    RectangularAperture,
+    EllipticalAperture,
+    PolygonAperture
+)
 from optiland.rays import RealRays
 
 
 class TestRadialAperture:
     def test_clip(self):
-        aperture = physical_apertures.RadialAperture(r_max=5, r_min=2)
+        aperture = RadialAperture(r_max=5, r_min=2)
         rays = RealRays([0, 1, 2, 3, 4, 5], [0, 1, 2, 3, 4, 5],
                         [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0],
                         [0, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 1],
@@ -14,18 +23,18 @@ class TestRadialAperture:
         assert np.all(rays.i == [0, 0, 1, 1, 0, 0])
 
     def test_scale(self):
-        aperture = physical_apertures.RadialAperture(r_max=5, r_min=2)
+        aperture = RadialAperture(r_max=5, r_min=2)
         aperture.scale(2)
         assert aperture.r_max == 10
         assert aperture.r_min == 4
 
-        aperture = physical_apertures.RadialAperture(r_max=5, r_min=2)
+        aperture = RadialAperture(r_max=5, r_min=2)
         aperture.scale(0.5)
         assert aperture.r_max == 2.5
         assert aperture.r_min == 1
 
     def test_to_dict(self):
-        aperture = physical_apertures.RadialAperture(r_max=5, r_min=2)
+        aperture = RadialAperture(r_max=5, r_min=2)
         assert aperture.to_dict() == {
             'type': 'RadialAperture',
             'r_max': 5,
@@ -38,15 +47,15 @@ class TestRadialAperture:
             'r_max': 5,
             'r_min': 2
         }
-        aperture = physical_apertures.RadialAperture.from_dict(data)
+        aperture = RadialAperture.from_dict(data)
         assert aperture.r_max == 5
         assert aperture.r_min == 2
-        assert isinstance(aperture, physical_apertures.RadialAperture)
+        assert isinstance(aperture, RadialAperture)
 
 
 class TestOffsetRadialAperture:
     def test_clip(self):
-        aperture = physical_apertures.OffsetRadialAperture(
+        aperture = OffsetRadialAperture(
             r_max=5, r_min=2, offset_x=1, offset_y=1
         )
         rays = RealRays([0, 1, 2, 3, 4, 5], [0, 1, 2, 3, 4, 5],
@@ -57,7 +66,7 @@ class TestOffsetRadialAperture:
         assert np.all(rays.i == [0, 0, 0, 1, 1, 0])
 
     def test_scale(self):
-        aperture = physical_apertures.OffsetRadialAperture(
+        aperture = OffsetRadialAperture(
             r_max=5, r_min=2, offset_x=1, offset_y=1
         )
         aperture.scale(2)
@@ -66,7 +75,7 @@ class TestOffsetRadialAperture:
         assert aperture.offset_x == 2
         assert aperture.offset_y == 2
 
-        aperture = physical_apertures.OffsetRadialAperture(
+        aperture = OffsetRadialAperture(
             r_max=5, r_min=2, offset_x=1, offset_y=1
         )
         aperture.scale(0.5)
@@ -76,7 +85,7 @@ class TestOffsetRadialAperture:
         assert aperture.offset_y == 0.5
 
     def test_to_dict(self):
-        aperture = physical_apertures.OffsetRadialAperture(
+        aperture = OffsetRadialAperture(
             r_max=5, r_min=2, offset_x=1, offset_y=1
         )
         assert aperture.to_dict() == {
@@ -95,9 +104,41 @@ class TestOffsetRadialAperture:
             'offset_x': 1,
             'offset_y': 1
         }
-        aperture = physical_apertures.OffsetRadialAperture.from_dict(data)
+        aperture = OffsetRadialAperture.from_dict(data)
         assert aperture.r_max == 5
         assert aperture.r_min == 2
         assert aperture.offset_x == 1
         assert aperture.offset_y == 1
-        assert isinstance(aperture, physical_apertures.OffsetRadialAperture)
+        assert isinstance(aperture, OffsetRadialAperture)
+
+
+class TestBooleanApertures:
+    def setup_method(self):
+        self.aperture1 = RadialAperture(r_max=1)
+        self.aperture2 = RadialAperture(r_max=0.5)
+
+    def test_union_aperture(self):
+        union_aperture = UnionAperture(self.aperture1, self.aperture2)
+        x = np.array([0, 0.0, 0.7, 1.5])
+        y = np.array([0, 0.5, 0.0, 1.5])
+        result = union_aperture.contains(x, y)
+        expected = np.array([True, True, True, False])
+        np.testing.assert_array_equal(result, expected)
+
+    def test_intersection_aperture(self):
+        intersection_aperture = IntersectionAperture(self.aperture1,
+                                                     self.aperture2)
+        x = np.array([0, 0.0, 0.7, 1.5])
+        y = np.array([0, 0.5, 0.0, 1.5])
+        result = intersection_aperture.contains(x, y)
+        expected = np.array([True, True, False, False])
+        np.testing.assert_array_equal(result, expected)
+
+    def test_difference_aperture(self):
+        difference_aperture = DifferenceAperture(self.aperture1,
+                                                 self.aperture2)
+        x = np.array([0, 0.0, 0.7, 1.5])
+        y = np.array([0, 0.5, 0.0, 1.5])
+        result = difference_aperture.contains(x, y)
+        expected = np.array([False, False, True, False])
+        np.testing.assert_array_equal(result, expected)
