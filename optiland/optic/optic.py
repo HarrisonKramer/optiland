07f64ed757e1a38b267858149a28baa2ece42d94
Kramer <kdanielharrison@gmail.com>
2025-03-29 11:00:29 +0100

    feat: use reset method in Optic class for system initialization

--- optiland/optic/optic.py
+++ optiland/optic/optic.py
@@ -50,6 +50,10 @@ class Optic:
 
     def __init__(self, name: str = None):
         self.name = name
+        self.reset()
+
+    def reset(self):
+        """Reset the optical system to its initial state."""
         self.aperture = None
         self.field_type = None
 
@@ -397,25 +401,6 @@ class Optic:
         """Display the optical system information."""
         viewer = LensInfoViewer(self)
         viewer.view()
-
-    def reset(self):
-        """Reset the optical system to its initial state."""
-        self.aperture = None
-        self.field_type = None
-
-        self.surface_group = SurfaceGroup()
-        self.fields = FieldGroup()
-        self.wavelengths = WavelengthGroup()
-
-        self.paraxial = Paraxial(self)
-        self.aberrations = Aberrations(self)
-        self.ray_generator = RayGenerator(self)
-
-        self.polarization = "ignore"
-
-        self.pickups = PickupManager(self)
-        self.solves = SolveManager(self)
-        self.obj_space_telecentric = False
 
     def n(self, wavelength="primary"):
         """Get the refractive indices of the surfaces.
