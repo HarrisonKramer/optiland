e8fa42a61b5eafa62cbb0d1db7ef864a30f5ce2c
Kramer <kdanielharrison@gmail.com>
2025-03-30 20:09:59 +0200

    feat: add OpticModifier and update ParaxialRayTracer documentation

--- optiland/raytrace/paraxial_ray_tracer.py
+++ optiland/raytrace/paraxial_ray_tracer.py
@@ -1,3 +1,18 @@
+"""Paraxial Ray Tracer Module
+
+This module contains the ParaxialRayTracer class, which is responsible for tracing
+paraxial rays through an optical system.
+
+Kramer Harrison, 2025
+"""
+
+
 class ParaxialRayTracer:
+    """Class to trace paraxial rays through an optical system
+
+    Args:
+        optic (Optic): The optical system to be traced.
+    """
+
     def __init__(self, optic):
         self.optic = optic
