3df6984462c643cce2a685bf7b8265c55eb720c1
Kramer <kdanielharrison@gmail.com>
2025-04-18 19:46:35 +0200

    fix: add set_test_backend fixture to all test methods in TestOptic class

--- tests/test_optic.py
+++ tests/test_optic.py
@@ -61,7 +61,7 @@ class TestOptic:
     def setup(self):
         self.optic = Optic()
 
-    def test_initialization(self):
+    def test_initialization(self, set_test_backend):
         assert self.optic.aperture is None
         assert self.optic.field_type is None
         assert isinstance(self.optic.surface_group, SurfaceGroup)
@@ -70,7 +70,7 @@ class TestOptic:
         assert self.optic.polarization == "ignore"
         assert not self.optic.obj_space_telecentric
 
-    def test_add_surface(self):
+    def test_add_surface(self, set_test_backend):
         self.optic.add_surface(
             index=0,
             surface_type="standard",
@@ -79,29 +79,29 @@ class TestOptic:
         )
         assert len(self.optic.surface_group.surfaces) == 1
 
-    def test_add_field(self):
+    def test_add_field(self, set_test_backend):
         self.optic.add_field(10.0, 5.0)
         assert len(self.optic.fields.fields) == 1
         assert self.optic.fields.fields[0].y == 10.0
         assert self.optic.fields.fields[0].x == 5.0
 
-    def test_add_wavelength(self):
+    def test_add_wavelength(self, set_test_backend):
         self.optic.add_wavelength(0.55, is_primary=True)
         assert len(self.optic.wavelengths.wavelengths) == 1
         assert self.optic.wavelengths.wavelengths[0].value == 0.55
         assert self.optic.wavelengths.wavelengths[0].is_primary
 
-    def test_set_aperture(self):
+    def test_set_aperture(self, set_test_backend):
         self.optic.set_aperture("EPD", 5.0)
         assert isinstance(self.optic.aperture, Aperture)
         assert self.optic.aperture.ap_type == "EPD"
         assert self.optic.aperture.value == 5.0
 
-    def test_set_field_type(self):
+    def test_set_field_type(self, set_test_backend):
         self.optic.set_field_type("angle")
         assert self.optic.field_type == "angle"
 
-    def test_set_comment(self):
+    def test_set_comment(self, set_test_backend):
         self.optic.add_surface(
             index=0,
             surface_type="standard",
@@ -120,7 +120,7 @@ class TestOptic:
         assert self.optic.surface_group.surfaces[0].comment == "Object surface"
         assert self.optic.surface_group.surfaces[1].comment == "First surface"
 
-    def test_set_radius(self):
+    def test_set_radius(self, set_test_backend):
         self.optic.add_surface(
             index=0,
             surface_type="standard",
@@ -130,7 +130,7 @@ class TestOptic:
         self.optic.set_radius(10.0, 0)
         assert self.optic.surface_group.surfaces[0].geometry.radius == 10.0
 
-    def test_set_conic(self):
+    def test_set_conic(self, set_test_backend):
         self.optic.add_surface(
             index=0,
             surface_type="standard",
@@ -140,7 +140,7 @@ class TestOptic:
         self.optic.set_conic(-1.0, 0)
         assert self.optic.surface_group.surfaces[0].geometry.k == -1.0
 
-    def test_set_thickness(self):
+    def test_set_thickness(self, set_test_backend):
         self.optic.add_surface(
             index=0,
             surface_type="standard",
@@ -162,7 +162,7 @@ class TestOptic:
         self.optic.set_thickness(10.0, 1)
         assert self.optic.surface_group.get_thickness(1) == 10.0
 
-    def test_set_index(self):
+    def test_set_index(self, set_test_backend):
         self.optic.add_surface(
             index=0,
             surface_type="standard",
@@ -184,7 +184,7 @@ class TestOptic:
         self.optic.set_index(1.5, 1)
         assert self.optic.surface_group.surfaces[1].material_post.n(1) == 1.5
 
-    def test_set_asphere_coeff(self):
+    def test_set_asphere_coeff(self, set_test_backend):
         self.optic.add_surface(
             index=0,
             surface_type="even_asphere",
@@ -195,15 +195,15 @@ class TestOptic:
         self.optic.set_asphere_coeff(0.1, 0, 2)
         assert self.optic.surface_group.surfaces[0].geometry.c[2] == 0.1
 
-    def test_set_polarization(self):
+    def test_set_polarization(self, set_test_backend):
         self.optic.set_polarization("ignore")
         assert self.optic.polarization == "ignore"
 
-    def test_set_invalid_polarization(self):
+    def test_set_invalid_polarization(self, set_test_backend):
         with pytest.raises(ValueError):
             self.optic.set_polarization("invalid")
 
-    def test_set_pickup(self):
+    def test_set_pickup(self, set_test_backend):
         self.optic.add_surface(
             index=0,
             surface_type="standard",
@@ -219,7 +219,7 @@ class TestOptic:
         self.optic.pickups.add(0, "radius", 1, scale=2, offset=1)
         assert len(self.optic.pickups) == 1
 
-    def test_clear_pickups(self):
+    def test_clear_pickups(self, set_test_backend):
         self.optic.add_surface(
             index=0,
             surface_type="standard",
@@ -236,18 +236,18 @@ class TestOptic:
         self.optic.pickups.clear()
         assert len(self.optic.pickups) == 0
 
-    def test_set_solve(self):
+    def test_set_solve(self, set_test_backend):
         optic = HeliarLens()
         optic.solves.add("marginal_ray_height", 6, height=10)
         assert len(optic.solves) == 1
 
-    def test_clear_solves(self):
+    def test_clear_solves(self, set_test_backend):
         optic = HeliarLens()
         optic.solves.add("marginal_ray_height", 6, height=10)
         optic.solves.clear()
         assert len(optic.solves) == 0
 
-    def test_scale_system(self):
+    def test_scale_system(self, set_test_backend):
         self.optic.add_surface(
             index=0,
             surface_type="standard",
@@ -260,7 +260,7 @@ class TestOptic:
         assert self.optic.surface_group.surfaces[0].geometry.radius == 2 * 10.0
         assert self.optic.aperture.value == 2 * 5.0
 
-    def test_reset(self):
+    def test_reset(self, set_test_backend):
         self.optic.add_surface(
             index=0,
             surface_type="standard",
@@ -277,7 +277,7 @@ class TestOptic:
         assert len(self.optic.pickups) == 0
         assert len(self.optic.solves) == 0
 
-    def test_n(self):
+    def test_n(self, set_test_backend):
         self.optic.add_surface(
             index=0,
             surface_type="standard",
@@ -289,57 +289,57 @@ class TestOptic:
         n_values = self.optic.n()
         assert len(n_values) == 1
 
-    def test_update_paraxial(self):
+    def test_update_paraxial(self, set_test_backend):
         lens = HeliarLens()
         lens.update_paraxial()
         # This test is just to ensure the method runs without error
 
-    def test_update(self):
+    def test_update(self, set_test_backend):
         lens = HeliarLens()
         lens.update()
         # This test is just to ensure the method runs without error
 
-    def test_image_solve(self):
+    def test_image_solve(self, set_test_backend):
         lens = HeliarLens()
         lens.image_solve()
         # This test is just to ensure the method runs without error
 
-    def test_trace(self):
+    def test_trace(self, set_test_backend):
         lens = HeliarLens()
         rays = lens.trace(0.0, 0.0, 0.55)
         assert rays is not None
 
-    def test_trace_generic(self):
+    def test_trace_generic(self, set_test_backend):
         lens = HeliarLens()
         rays = lens.trace_generic(0.0, 0.0, 0.0, 0.0, 0.55)
         assert rays is not None
 
-    def test_trace_invalid_field(self):
+    def test_trace_invalid_field(self, set_test_backend):
         lens = HeliarLens()
         with pytest.raises(ValueError):
             lens.trace(0.0, 2.0, 0.55)
 
-    def test_trace_generic_invalid_field(self):
+    def test_trace_generic_invalid_field(self, set_test_backend):
         lens = HeliarLens()
         with pytest.raises(ValueError):
             lens.trace_generic(0.0, 0.0, 0.0, 2.0, 0.55)
 
-    def test_trace_generic_invalid_pupil(self):
+    def test_trace_generic_invalid_pupil(self, set_test_backend):
         lens = HeliarLens()
         with pytest.raises(ValueError):
             lens.trace_generic(0.0, 5.0, 0.0, 0.0, 0.55)
 
-    def test_trace_polarized(self):
+    def test_trace_polarized(self, set_test_backend):
         lens = HeliarLens()
         state = create_polarization("unpolarized")
         lens.set_polarization(state)
         rays = lens.trace(0.0, 0.0, 0.55)
         assert rays is not None
 
-    def test_object_property(self):
+    def test_object_property(self, set_test_backend):
         assert self.optic.object_surface is None
 
-    def test_image_surface_property(self):
+    def test_image_surface_property(self, set_test_backend):
         self.optic.add_surface(
             index=0,
             surface_type="standard",
@@ -349,18 +349,18 @@ class TestOptic:
         )
         assert self.optic.image_surface is self.optic.surface_group.surfaces[0]
 
-    def test_total_track_property(self):
+    def test_total_track_property(self, set_test_backend):
         lens = HeliarLens()
         assert lens.total_track == 3.6291
 
-    def test_total_track_error(self):
+    def test_total_track_error(self, set_test_backend):
         lens = HeliarLens()
         # manually remove all but first surface
         lens.surface_group.surfaces = [lens.surface_group.surfaces[0]]
         with pytest.raises(ValueError):
             _ = lens.total_track
 
-    def test_polarization_state_property(self):
+    def test_polarization_state_property(self, set_test_backend):
         lens = HeliarLens()
         assert lens.polarization_state is None
 
@@ -368,35 +368,35 @@ class TestOptic:
         lens.set_polarization(state)
         assert lens.polarization_state == state
 
-    def test_polarization_state_error(self):
+    def test_polarization_state_error(self, set_test_backend):
         lens = HeliarLens()
         lens.polarization = "invalid"
         with pytest.raises(ValueError):
             _ = lens.polarization_state
 
-    def test_to_dict(self):
+    def test_to_dict(self, set_test_backend):
         lens = HeliarLens()
         lens_dict = lens.to_dict()
         assert lens_dict is not None
 
-    def test_from_dict(self):
+    def test_from_dict(self, set_test_backend):
         lens = HeliarLens()
         lens_dict = lens.to_dict()
         new_lens = HeliarLens.from_dict(lens_dict)
         assert new_lens is not None
         assert new_lens.total_track == lens.total_track
 
-    def test_invalid_field_type(self):
+    def test_invalid_field_type(self, set_test_backend):
         with pytest.raises(ValueError):
             self.optic.set_field_type("invalid")
 
-    def test_no_stop(self):
+    def test_no_stop(self, set_test_backend):
         for surface in self.optic.surface_group.surfaces:
             surface.is_stop = False
         with pytest.raises(ValueError):
             _ = self.optic.surface_group.stop_index
 
-    def test_add_infinite_object(self):
+    def test_add_infinite_object(self, set_test_backend):
         lens1 = singlet_infinite_object()
         lens2 = singlet_infinite_object()
         lens_combined = lens1 + lens2
@@ -408,7 +408,7 @@ class TestOptic:
         )
         assert rays is not None
 
-    def test_add_finite_object(self):
+    def test_add_finite_object(self, set_test_backend):
         lens1 = singlet_finite_object()
         lens2 = singlet_finite_object()
         lens_combined = lens1 + lens2
