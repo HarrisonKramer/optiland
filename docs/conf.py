3e0ab42aa0267d60b2d54ec4f46785adf5714b4a
Kramer <kdanielharrison@gmail.com>
2025-05-03 16:32:26 +0200

    bump: version to 0.4.1

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@ project = "Optiland"
 current_year = datetime.now().year
 copyright = f"2024-{current_year}, Kramer Harrison & contributors"
 author = "Kramer Harrison"
-release = "0.4.0"
+release = "0.4.1"
 
 extensions = [
     "sphinx.ext.autodoc",
