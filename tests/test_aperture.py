f7aa3ad4d5ef1f5e0cbe5001332ec41f847b42aa
Kramer <kdanielharrison@gmail.com>
2025-05-13 21:01:35 +0200

    Feature/float by stop size (#149)
    * feat: extend aperture scaling to include 'float_by_stop_size' type
    
    * feat: add 'float_by_stop_size' as a valid aperture type and update validation logic
    
    * feat: add support for 'float_by_stop_size' aperture type in calculations
    
    * feat: add support for 'float_by_stop_size' aperture type in calculations. Fixes #103
    
    * chore: remove stale issue workflow configuration
    
    * feat: enhance 'float_by_stop_size' support in Aperture and add corresponding tests
    
    * docs: update aperture documentation to include 'float_by_stop_size' option
    
    * feat: add numpy import and update ndarray definition for backend compatibility
    
    * fix: undo changes in backend, which break many things...
    
    * docs: add float by stop size example

--- tests/test_aperture.py
+++ tests/test_aperture.py
@@ -1,11 +1,14 @@
 import pytest
 
+import optiland.backend as be
+from optiland.optic import Optic
 from optiland import aperture
+from .utils import assert_allclose
 
 
 @pytest.mark.parametrize(
     "ap_type, value",
-    [("EPD", 10), ("imageFNO", 3.2), ("objectNA", 0.265)],
+    [("EPD", 10), ("imageFNO", 3.2), ("objectNA", 0.265), ("float_by_stop_size", 1.142857)],
 )
 def test_aperture_generate(set_test_backend, ap_type, value):
     """Check instantiation of aperture"""
@@ -49,3 +52,28 @@ def test_invalid_from_dict(set_test_backend):
         aperture.Aperture.from_dict(
             {"invalid": "I am invalid, unfortunately", "value": 5.0},
         )
+
+
+def test_confirm_stop_size_floating_stop(set_test_backend):
+    lens = Optic()
+
+    lens.add_surface(index=0, radius=be.inf, thickness=be.inf)
+    lens.add_surface(index=1, radius=22.01359, thickness=3.25896, material="SK16")
+    lens.add_surface(index=2, radius=-435.76044, thickness=30, is_stop=True)
+    lens.add_surface(index=3)
+
+    stop_diam = 8.4
+    stop_idx = 2
+    lens.set_aperture(aperture_type="float_by_stop_size", value=stop_diam)
+
+    lens.set_field_type(field_type="angle")
+    lens.add_field(y=0)
+    lens.add_field(y=14)
+    lens.add_field(y=20)
+
+    lens.add_wavelength(value=0.48)
+    lens.add_wavelength(value=0.55, is_primary=True)
+    lens.add_wavelength(value=0.65)
+
+    lens.paraxial.trace(Hy=0, Py=1, wavelength=0.55)
+    assert_allclose(lens.surface_group.y[stop_idx], stop_diam / 2)
