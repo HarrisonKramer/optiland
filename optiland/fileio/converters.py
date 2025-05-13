e908cd156ecfe667ce7dc53b26855b13f8094556
Manuel Fragata <mambfm09@gmail.com>
2025-05-13 19:06:14 +0200

    Feat/coordinate breaks zmx (#148)
    * feat: coordinate breaks reading from zmx file
    
    -> first steps, provide the 'CORB' operand to the ZemaxfileReader operand table.
    
    -> create private method _read_coordinate_break to implement logic for reading coordinate break data
    
    * fix: remove unnecessary coordinate_break reading method
    
    * refactor: improve parameteer handling in Zemax to Optic converter
    
    * map: coord break from zmx to _create_plane.
    
    to-do: make sure the mirrors are correctly read from zmx files
    
    * feat: add suport for reading mirror material type from zemax
    
    * fix: remove unnecessary creation of surface when reading zmx coord breaks
    
    * chore: remove unnecessary debug notebooks
    
    * feat: track the current cs when a zmx file contains coord break surfaces. Otherwise follows the previously working logic. Some things still left to debug
    
    * fix: create an intermediate cs_rotations_translations, as per convention.
    
    -> the thickness in the cb surface type is applied only after the decenters/rotations are taken into effect
    
    * chore: add new zmx test system
    
    * fix: modify order of rotations in localize and globalize methods
    
    * fix: replace test assertions in test_coordinate_system.py
    
    * chore: formatting
    
    * tests: new fixtures for the complicated zmx system
    
    * tests: improve test coverage with additional tests for the complicated system with multiple coord breaks.
    
    * tests: generate complicated lens system test

--- optiland/fileio/converters.py
+++ optiland/fileio/converters.py
@@ -6,6 +6,8 @@ including Zemax and Optiland (.json) files.
 Kramer Harrison, 2024
 """
 
+import optiland.backend as be
+from optiland.coordinate_system import CoordinateSystem
 from optiland.optic import Optic
 
 
@@ -19,6 +21,9 @@ class ZemaxToOpticConverter:
     Attributes:
         data (dict): The Zemax data to be converted.
         optic (Optic): The Optic object based on the Zemax data.
+        current_cs (CoordinateSystem): Running, cumulative coordinate system
+            that tracks the decentres / tilts defined by any
+            preceding coordinate_break surface in the Zemax file.
 
     Methods:
         convert(): Converts the configuration of the file handler into an
@@ -29,6 +34,7 @@ class ZemaxToOpticConverter:
     def __init__(self, zemax_data):
         self.data = zemax_data
         self.optic = None
+        self.current_cs = CoordinateSystem()
 
     def convert(self):
         """Converts the configuration of the file handler into an Optic object.
@@ -46,13 +52,121 @@ class ZemaxToOpticConverter:
 
     def _configure_surfaces(self):
         """Configures the surfaces for the optic."""
-        for idx, surf_data in self.data["surfaces"].items():
-            self._configure_surface(idx, surf_data)
-        self.optic.add_surface(index=len(self.data["surfaces"]))
+        has_cb = any(
+            sd.get("type") == "coordinate_break"
+            for sd in self.data["surfaces"].values()
+        )
+
+        if not has_cb:
+            for idx, surf_data in self.data["surfaces"].items():
+                self._configure_surface(idx, surf_data)
+            self.optic.add_surface(index=len(self.data["surfaces"]))
+            return
+
+        # in case there are "Coordinate Break" surfaces
+        surf_idx = 0
+
+        for idx in sorted(self.data["surfaces"].keys(), key=int):
+            surf = self.data["surfaces"][idx]
+
+            # CB: we update the CS only, no added surface
+            if surf.get("type") == "coordinate_break":
+                dx = float(surf.get("param_0", 0.0))
+                dy = float(surf.get("param_1", 0.0))
+                dz = float(surf.get("thickness", 0.0))  # CB 'thickness'
+                rx = be.deg2rad(surf.get("param_2", 0.0))
+                ry = be.deg2rad(surf.get("param_3", 0.0))
+                rz = be.deg2rad(surf.get("param_4", 0.0))
+                # there is another param: order. implement later
+
+                # chain a new cs
+                # first apply rotations and translations
+                cs_rot_decs = CoordinateSystem(
+                    x=dx, y=dy, z=0.0, rx=rx, ry=ry, rz=rz, reference_cs=self.current_cs
+                )
+                # then apply the coordinate break's thickness as a translation
+                # along the Z-axis of the new cs.
+                self.current_cs = CoordinateSystem(
+                    x=0.0, y=0.0, z=dz, reference_cs=cs_rot_decs
+                )
+
+                continue
+
+            # now, the usual surfaces from the file
+            # transform into global CS, then append to optic
+            translation, _ = self.current_cs.get_effective_transform()
+            rx_, ry_, rz_ = self.current_cs.get_effective_rotation_euler()
+            coeffs = self._configure_surface_coefficients(surf)
+            thickness = surf.get("thickness", 0.0)
+
+            # special care now, we ramify. for object surface,
+            # if DISZ == inf, then keep infinity
+            if be.isinf(float(thickness)):
+                self.optic.add_surface(
+                    index=surf_idx,
+                    surface_type=surf["type"],
+                    radius=surf.get("radius"),
+                    conic=surf.get("conic"),
+                    thickness=thickness,
+                    is_stop=surf.get("is_stop", False),
+                    material=surf.get("material"),
+                    coefficients=coeffs,
+                    rx=float(rx_),
+                    ry=float(ry_),
+                    rz=float(rz_),
+                )
+            else:  # normal surface, we pass no thickness argument
+                self.optic.add_surface(
+                    index=surf_idx,
+                    surface_type=surf["type"],
+                    radius=surf.get("radius"),
+                    conic=surf.get("conic"),
+                    is_stop=surf.get("is_stop", False),
+                    material=surf.get("material"),
+                    coefficients=coeffs,
+                    x=float(translation[0]),
+                    y=float(translation[1]),
+                    z=float(translation[2]),
+                    rx=float(rx_),
+                    ry=float(ry_),
+                    rz=float(rz_),
+                )
+            surf_idx = surf_idx + 1
+
+            # we need to advance the cs by the surface thickness
+            # if it is finite
+            dt = surf.get("thickness", 0.0)
+            if not be.isinf(dt):
+                self.current_cs = CoordinateSystem(
+                    x=0.0, y=0.0, z=dt, reference_cs=self.current_cs
+                )
+
+        # image surface specific
+        translation, _ = self.current_cs.get_effective_transform()
+        rx_, ry_, rz_ = self.current_cs.get_effective_rotation_euler()
+        self.optic.add_surface(
+            index=surf_idx,
+            x=translation[0],
+            y=translation[1],
+            z=translation[2],
+            rx=rx_,
+            ry=ry_,
+            rz=rz_,
+        )
 
     def _configure_surface(self, index, data):
         """Configures a surface for the optic."""
         coefficients = self._configure_surface_coefficients(data)
+        extra_params = {}
+        if data["type"] == "coordinate_break":
+            # map the zmx PARM values to the actual decenters and rotations
+            extra_params["dx"] = data.get("param_0", 0.0)
+            extra_params["dy"] = data.get("param_1", 0.0)
+            # convert degrees to radians
+            extra_params["rx"] = be.deg2rad(be.array(data.get("param_2", 0.0)))
+            extra_params["ry"] = be.deg2rad(be.array(data.get("param_3", 0.0)))
+            extra_params["rz"] = be.deg2rad(be.array(data.get("param_4", 0.0)))
+            extra_params["order_flag"] = data.get("param_5", 0.0)
         self.optic.add_surface(
             index=index,
             surface_type=data["type"],
@@ -62,6 +176,7 @@ class ZemaxToOpticConverter:
             is_stop=data["is_stop"],
             material=data["material"],
             coefficients=coefficients,
+            **extra_params,
         )
 
     def _configure_surface_coefficients(self, data):
@@ -69,7 +184,7 @@ class ZemaxToOpticConverter:
         surfaces.
         """
         surf_type = data["type"]
-        if surf_type == "standard":
+        if surf_type == "standard" or surf_type == "coordinate_break":
             return None
         if surf_type in ["even_asphere", "odd_asphere"]:
             coefficients = []
