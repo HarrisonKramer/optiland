43a181fa8301c34c8857af48f2ba5cd68ad811dd
Kramer <kdanielharrison@gmail.com>
2025-07-26 11:28:56 +0200

    bump: to version 0.5.4

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@ project = "Optiland"
 current_year = datetime.now().year
 copyright = f"2024-{current_year}, Kramer Harrison & contributors"
 author = "Kramer Harrison"
-release = "0.5.3"
+release = "0.5.4"
 
 extensions = [
     "jupyterlite_sphinx",
