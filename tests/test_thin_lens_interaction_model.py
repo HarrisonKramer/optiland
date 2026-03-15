d93b215f61c9adc357e5b4149467d2a190d73090
Robert Moerland <r.moerland@lumicks.com>
2026-03-15 13:08:59 +0100

    combine refractive check and perfect imaging
    # Conflicts:
    #	tests/test_thin_lens_interaction_model.py

--- tests/test_thin_lens_interaction_model.py
+++ tests/test_thin_lens_interaction_model.py
@@ -72,16 +72,30 @@ class TestThinLensInteractionModel:
         traced_rays = surface.trace(rays)
         assert isinstance(traced_rays, RealRays)
 
-    def test_paraxial_surface_perfect_imaging(self, set_test_backend):
+    @pytest.mark.parametrize(
+        "focal_length, n1, n2",
+        [
+            (50.0, 1.0, 1.0),
+            (50.0, 1.5, 2.0),
+            (50.0, 2.0, 1.5),
+            (-50.0, 1.0, 1.0),
+            (-50.0, 1.5, 2.0),
+            (-50.0, 2.0, 1.5),
+        ],
+    )
+    def test_paraxial_surface_perfect_imaging_refraction(
+        self, focal_length, n1, n2, set_test_backend
+    ):
         lens = Optic()
 
         # add surfaces
-        lens.surfaces.add(index=0, thickness=be.inf)
+        lens.surfaces.add(index=0, thickness=be.inf, material=IdealMaterial(n1))
         lens.surfaces.add(
             index=1,
             surface_type="paraxial",
-            thickness=100,
-            f=100,
+            material=IdealMaterial(n2),
+            thickness=focal_length * n2,
+            f=focal_length,
             is_stop=True,
         )
         lens.surfaces.add(index=2)
@@ -92,7 +106,6 @@ class TestThinLensInteractionModel:
         # add field
         lens.fields.set_type(field_type="angle")
         lens.fields.add(y=0)
-        # lens.fields.add(y=5)
 
         # add wavelength
         lens.wavelengths.add(value=0.55, is_primary=True)
@@ -109,7 +122,7 @@ class TestThinLensInteractionModel:
         assert_allclose(rays.y, 0)
 
         # confirm all points at exact same z
-        assert_allclose(rays.z, 100)
+        assert_allclose(rays.z, focal_length * n2)
 
     def test_flip(self, surface):
         f_initial = be.copy(surface.interaction_model.f)
